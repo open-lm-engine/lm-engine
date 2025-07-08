f01e17f2e8f0d193ab0bbf49a7971b7c4a98c166
Mayank Mishra <mayank31398@gmail.com>
2025-07-08 10:25:42 +0530

    drop CLA tests
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/models/gpt_crosslayer/__init__.py
+++ lm_engine/hf_models/models/gpt_crosslayer/__init__.py
@@ -5,4 +5,3 @@
 from .base import GPTCrossLayerModel, GPTCrossLayerPreTrainedModel
 from .config import GPTCrossLayerConfig
 from .main import GPTCrossLayerForCausalLM
-from .utils import convert_gpt_base_to_gpt_crosslayer
