cc7aa89c6e10de2a0f02864d7c434ec6622f13fc
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2025-07-08 21:32:08 -0400

    drop token_type_ids (#260)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/utils.py
+++ lm_engine/hf_models/utils.py
@@ -11,7 +11,6 @@ def convert_padding_free_lists_to_tensors(
     input_ids: list[list[int]] | None = None,
     inputs_embeds: list[list[float]] | None = None,
     position_ids: list[list[int]] | None = None,
-    token_type_ids: list[list[int]] | None = None,
     labels: list[list[int]] | None = None,
     device: torch.device = None,
 ) -> tuple[torch.Tensor | int]:
@@ -21,7 +20,6 @@ def convert_padding_free_lists_to_tensors(
     _check_list_type(input_ids, error_message.format(variable="input_ids", dtype="int"))
     _check_list_type(inputs_embeds, error_message.format(variable="inputs_embeds", dtype="float"))
     _check_list_type(position_ids, error_message.format(variable="position_ids", dtype="int"))
-    _check_list_type(token_type_ids, error_message.format(variable="token_type_ids", dtype="int"))
     _check_list_type(labels, error_message.format(variable="labels", dtype="int"))
 
     # prepare inputs for the model
@@ -39,13 +37,10 @@ def convert_padding_free_lists_to_tensors(
     if inputs_embeds is not None:
         inputs_embeds = _flatten_and_convert_to_tensors(inputs_embeds, device)
 
-    if token_type_ids is not None:
-        token_type_ids = _flatten_and_convert_to_tensors(token_type_ids, device)
-
     if labels is not None:
         labels = _flatten_and_convert_to_tensors(labels, device)
 
-    return input_ids, position_ids, token_type_ids, labels, cu_seqlens, max_seqlen
+    return input_ids, position_ids, labels, cu_seqlens, max_seqlen
 
 
 def _check_list_type(list_of_list: list[list[int | float]] | None, error_message: str) -> None:
