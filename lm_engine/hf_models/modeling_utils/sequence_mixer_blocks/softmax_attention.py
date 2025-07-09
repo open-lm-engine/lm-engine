ca7fc80173a35fb7718a74dbf586ec2aede4c8c9
Mayank Mishra <mayank31398@gmail.com>
2025-07-09 05:33:43 +0530

    drop softmax_scale
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/softmax_attention.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/softmax_attention.py
@@ -226,35 +226,19 @@ class Attention(nn.Module):
             f"`hidden_size` ({self.hidden_size}) must be divisible by `num_heads` ({self.num_heads})",
         )
 
-        self.attention_head_type = get_attention_head_type(num_attention_heads, num_key_value_heads)
         self.position_embedding_type = position_embedding_type
         self.attention_multiplier = attention_multiplier
         self.layer_idx = layer_idx
 
-        if self.attention_head_type == "mha":
-            if self.num_key_value_heads is None:
-                self.num_key_value_heads = self.num_heads
+        assert (
+            self.num_key_value_heads is not None
+        ), "`num_key_value_heads` needs to be specified with GroupedQueryAttention"
 
-            assert (
-                self.num_heads == self.num_key_value_heads
-            ), f"{self.__class__.__name__} should have same number of heads for query, keys and values"
-        elif self.attention_head_type == "gqa":
-            assert (
-                self.num_key_value_heads is not None
-            ), "`num_key_value_heads` needs to be specified with GroupedQueryAttention"
-
-            divide_if_divisible(
-                self.num_heads,
-                self.num_key_value_heads,
-                f"`num_heads` ({self.num_heads}) should be a multiple of `num_key_value_heads` ({self.num_key_value_heads})",
-            )
-        elif self.attention_head_type == "mqa":
-            if self.num_key_value_heads is None:
-                self.num_key_value_heads = 1
-
-            assert self.num_key_value_heads == 1, f"{self.__class__.__name__} should have 1 head for keys and values"
-        else:
-            raise ValueError(f"unexpected attention_head_type ({self.attention_head_type})")
+        divide_if_divisible(
+            self.num_heads,
+            self.num_key_value_heads,
+            f"`num_heads` ({self.num_heads}) should be a multiple of `num_key_value_heads` ({self.num_key_value_heads})",
+        )
 
         # note that the actual layout is different for the output and depends on whether we are using MHA, MQA or GQA
         # (self.hidden_size + 2 * self.num_key_value_heads * self.head_dim) is just the actual number output features
@@ -281,32 +265,6 @@ class Attention(nn.Module):
         mark_parameter_as_mup_learning_rate(self.c_attn.weight)
         mark_parameter_as_mup_learning_rate(self.c_proj.weight)
 
-    def _prepare_qkv_for_forward(self, hidden_states: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        # the output of following is a tuple if using MQA with tensor parallel
-        hidden_states = self.c_attn(hidden_states)
-
-        # for MHA, we can get away with doing just 1 transpose which is not true for GQA
-        if self.attention_head_type == "mha":
-            query, key, value = self._prepare_qkv_for_forward_mha(hidden_states)
-        elif self.attention_head_type == "gqa":
-            query, key, value = self._prepare_qkv_for_forward_gqa(hidden_states)
-        elif self.attention_head_type == "mqa":
-            query, key, value = self._prepare_qkv_for_forward_mqa(hidden_states)
-        else:
-            raise ValueError(f"unexpected attention_head_type ({self.attention_head_type})")
-
-        return query, key, value
-
-    def _prepare_qkv_for_forward_mha(
-        self, hidden_states: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        T = hidden_states.size(0)
-        hidden_states = hidden_states.view(T, self.num_key_value_heads, -1)
-
-        query, key, value = hidden_states.chunk(3, dim=-1)
-
-        return query, key, value
-
     def _prepare_qkv_for_forward_gqa(
         self, hidden_states: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
@@ -322,19 +280,6 @@ class Attention(nn.Module):
 
         return query, key, value
 
-    def _prepare_qkv_for_forward_mqa(
-        self, hidden_states: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        T = hidden_states.size(0)
-
-        query, key, value = hidden_states.split((self.hidden_size, self.head_dim, self.head_dim), dim=-1)
-
-        query = query.view(T, self.num_heads, -1)
-        key = key.unsqueeze(1)
-        value = value.unsqueeze(1)
-
-        return query, key, value
-
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -346,7 +291,15 @@ class Attention(nn.Module):
     ) -> torch.Tensor:
         assert past_key_values is None
 
-        query, key, value = self._prepare_qkv_for_forward(hidden_states)
+        T = hidden_states.size(0)
+        hidden_states = self.c_attn(hidden_states)
+
+        hidden_states = hidden_states.view(T, self.num_key_value_heads, -1)
+        query, key, value = hidden_states.split(
+            ((self.num_heads // self.num_key_value_heads) * self.head_dim, self.head_dim, self.head_dim), dim=-1
+        )
+
+        query = query.reshape(T, -1, self.head_dim)
 
         if self.position_embedding_type == "rope":
             query = apply_rotary_pos_emb(query, rope_cos_sin)
