e38d98120510e0e44332617c026521a797ac4072
Mayank Mishra <mayank31398@gmail.com>
2025-07-16 15:09:20 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/models/gpt_base_TP/weights/fix.py
+++ lm_engine/hf_models/models/gpt_base_TP/weights/fix.py
@@ -18,7 +18,7 @@ def fix_gpt_base_unsharded_state_dict(
     for layer_idx in range(config.num_layers):
         block = config.sequence_mixer_blocks[layer_idx]
 
-        if block.num_key_value_heads == 1:
+        if block.num_attention_heads > 1 and block.num_key_value_heads == 1:
             q_attn_w = state_dict.pop(f"{prefix}transformer.h.{layer_idx}.sequence_mixer.c_attn.q_attn.weight")
             kv_attn_w = state_dict.pop(f"{prefix}transformer.h.{layer_idx}.sequence_mixer.c_attn.kv_attn.weight")
             state_dict[f"{prefix}transformer.h.{layer_idx}.sequence_mixer.c_attn.weight"] = torch.cat(
