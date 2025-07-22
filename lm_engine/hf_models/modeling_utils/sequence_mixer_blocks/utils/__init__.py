83baa8e7b12a833b8ca10eda0925333dcffe69dc
Mayank Mishra <mayank31398@gmail.com>
2025-07-22 18:28:30 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/utils/__init__.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/utils/__init__.py
@@ -2,6 +2,5 @@
 # Copyright (c) 2025, Mayank Mishra
 # **************************************************
 
-from .flash import flash_attention
 from .flash_attention_utils import flash_attention
 from .packing import compute_cu_seqlens_and_max_seqlen_from_attention_mask, pack_sequence, unpack_sequence
