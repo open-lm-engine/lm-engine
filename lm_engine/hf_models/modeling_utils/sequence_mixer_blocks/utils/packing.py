81ac3139a3332d02ba19798f5851e98ca6517980
Mayank Mishra <mayank31398@gmail.com>
2025-07-28 14:29:58 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/utils/packing.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/utils/packing.py
@@ -11,7 +11,9 @@ from .....utils import is_fma_available
 
 
 if is_fma_available():
-    from fma import KernelBackend, pack_sequence, unpack_sequence
+    from fma import KernelBackend
+    from fma import pack_sequence as _pack_sequence
+    from fma import unpack_sequence as _unpack_sequence
 
 
 def compute_cu_seqlens_and_max_seqlen_from_attention_mask(
@@ -28,7 +30,7 @@ def pack_sequence(
 ) -> torch.Tensor | list[torch.Tensor]:
     kernel_backend = KernelBackend.cuda if is_kernel_allowed(Kernel.pack_sequence) else KernelBackend.torch
 
-    inputs = pack_sequence(
+    inputs = _pack_sequence(
         inputs=inputs,
         cu_seqlens=cu_seqlens,
         kernel_backend_forward=kernel_backend,
@@ -43,7 +45,7 @@ def unpack_sequence(
 ) -> torch.Tensor | list[torch.Tensor]:
     kernel_backend = KernelBackend.cuda if is_kernel_allowed(Kernel.unpack_sequence) else KernelBackend.torch
 
-    inputs = unpack_sequence(
+    inputs = _unpack_sequence(
         inputs=inputs,
         cu_seqlens=cu_seqlens,
         output_shape=output_shape,
