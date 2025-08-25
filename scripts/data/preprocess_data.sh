d3117f3927d906999d61b9aa699caf66f86f7ff3
Mayank Mishra <mayank31398@gmail.com>
2025-08-25 09:50:44 -0700

    file tokenization
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ scripts/data/preprocess_data.sh
@@ -0,0 +1,10 @@
+INPUT_PATH=data.jsonl
+OUTPUT_PATH=data
+TOKENIZER=ibm-granite/granite-3b-code-base
+
+python tools/data/preprocess_data.py \
+    --input $INPUT_PATH \
+    --tokenizer $TOKENIZER \
+    --output-prefix $OUTPUT_PATH \
+    --workers 1 \
+    --chunk-size 1000
