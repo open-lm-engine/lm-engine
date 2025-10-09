d4ac5cb42fae40e1038c6d2897482b67fd7579c6
Mayank Mishra <mayank31398@gmail.com>
2025-10-08 17:12:21 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/single_gpu/gpt_base_test.py
+++ tests/hf_models/single_gpu/gpt_base_test.py
@@ -2,8 +2,6 @@
 # Copyright (c) 2025, Mayank Mishra
 # **************************************************
 
-import itertools
-
 import torch
 from parameterized import parameterized
 from transformers import set_seed
@@ -105,51 +103,6 @@ class GPTBaseAttentionTest(TestCommons):
             atol_bfloat16=5e-3,
         )
         self.assert_equal_tensors(sdpa_loss, flash_loss, False, atol_float32=1.2e-4, rtol_float32=0)
-
-    @parameterized.expand(
-        TestCommons.make_args_matrix(
-            [torch.device("cuda")], TestCommons.get_position_embedding_types(), [torch.float16, torch.bfloat16]
-        )
-    )
-    def test_padding_free_transformer_with_list_and_tensor(
-        self, device: torch.device, position_embedding_type: str, dtype: torch.dtype
-    ) -> None:
-        self.skip_test_if_device_unavailable(device)
-
-        set_seed(SEED)
-
-        config = self.get_dense_test_config(position_embedding_type, num_layers=1)
-
-        model = self.from_config(config, dtype=dtype).to(device)
-        model.eval()
-
-        with enable_kernels([Kernel.flash_attention_2]):
-            input_ids, attention_mask, labels = self.get_dummy_inputs(device, return_list=True)
-            list_output = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-            list_logits = list_output.logits
-            list_loss = list_output.loss
-
-            seqlens = torch.tensor([0] + [len(i) for i in input_ids])
-            cu_seqlens = seqlens.cumsum(dim=-1).to(device, torch.int32)
-            max_seqlen = seqlens.max().item()
-            position_ids = torch.tensor(
-                list(itertools.chain(*[list(range(len(i))) for i in input_ids])), device=device
-            )
-            input_ids = torch.tensor(list(itertools.chain(*input_ids)), device=device)
-            labels = torch.tensor(list(itertools.chain(*labels)), device=device)
-            tensor_output = model(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                position_ids=position_ids,
-                labels=labels,
-                cu_seqlens=cu_seqlens,
-                max_seqlen=max_seqlen,
-            )
-            tensor_logits = tensor_output.logits
-            tensor_loss = tensor_output.loss
-
-        self.assert_equal_tensors(list_logits, tensor_logits, True)
-        self.assert_equal_tensors(list_loss, tensor_loss, True)
 
     @parameterized.expand(
         TestCommons.make_args_matrix(
