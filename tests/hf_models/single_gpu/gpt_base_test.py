fcf6761254c0ea83ad638a44e0921bd56ef481a1
Mayank Mishra <mayank31398@gmail.com>
2025-10-08 17:10:01 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/single_gpu/gpt_base_test.py
+++ tests/hf_models/single_gpu/gpt_base_test.py
@@ -156,43 +156,6 @@ class GPTBaseAttentionTest(TestCommons):
             [torch.device("cuda")], TestCommons.get_position_embedding_types(), [torch.float16, torch.bfloat16]
         )
     )
-    def test_sdpa_flash_enabled(self, device: torch.device, position_embedding_type: str, dtype: torch.dtype) -> None:
-        self.skip_test_if_device_unavailable(device)
-
-        set_seed(SEED)
-
-        config = self.get_dense_test_config(position_embedding_type, num_layers=1)
-
-        model = self.from_config(config, dtype=dtype).to(device)
-        model.eval()
-
-        input_ids, _, labels = self.get_dummy_inputs(device)
-        attention_mask = torch.ones_like(input_ids, dtype=torch.int, device=device)
-
-        sdpa_output = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-        sdpa_logits = sdpa_output.logits
-        sdpa_loss = sdpa_output.loss
-
-        flash_output = model(input_ids=input_ids, labels=labels)
-        flash_logits = flash_output.logits
-        flash_loss = flash_output.loss
-
-        self.assert_equal_tensors(
-            sdpa_logits,
-            flash_logits,
-            False,
-            rtol_float16=1e-3,
-            atol_float16=3e-4,
-            rtol_bfloat16=5e-3,
-            atol_bfloat16=5e-3,
-        )
-        self.assert_equal_tensors(sdpa_loss, flash_loss, False, atol_float32=3.8e-4, rtol_float32=0)
-
-    @parameterized.expand(
-        TestCommons.make_args_matrix(
-            [torch.device("cuda")], TestCommons.get_position_embedding_types(), [torch.float16, torch.bfloat16]
-        )
-    )
     def test_flash_attention_equivalence_with_and_without_attention_masks(
         self, device: torch.device, position_embedding_type: str, dtype: torch.dtype
     ) -> None:
