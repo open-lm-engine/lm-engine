c93232aa3ef66c623acdd73b8ab92c79760de863
Mayank Mishra <mayank31398@gmail.com>
2025-10-09 12:21:11 -0700

    better
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/mask.py
+++ lm_engine/hf_models/mask.py
@@ -28,26 +28,27 @@ class AttentionMaskInfo:
     cu_seqlens: torch.Tensor | None = None
     max_seqlen: int | None = None
     attention_mask: torch.Tensor | None = None
-    _causal_mask: torch.Tensor | None = None
     device: torch.device | None = None
     mask_value: torch.Tensor | None = None
+    causal_mask: torch.Tensor | None = None
 
     def __post_init__(self) -> None:
         self._has_cu_seqlens = self.cu_seqlens is not None
+        self._has_attention_mask = self.attention_mask is not None
 
         if self.batch_size is not None:
             assert self.max_seqlen is not None
-            assert self.cu_seqlens is None
-            assert self.attention_mask is None
+            assert not self.has_cu_seqlens()
+            assert not self.has_attention_mask()
         elif self.cu_seqlens is not None:
             assert self.batch_size is None
             assert self.max_seqlen is not None
-            assert self.attention_mask is None
+            assert not self.has_attention_mask()
 
             self.device = self.cu_seqlens.device
-        elif self.attention_mask is not None:
+        elif self.has_attention_mask():
             assert self.batch_size is None
-            assert self.cu_seqlens is None
+            assert not self.has_cu_seqlens()
             assert self.max_seqlen is None
 
             self.device = self.attention_mask.device
@@ -57,40 +58,49 @@ class AttentionMaskInfo:
     def has_cu_seqlens(self) -> bool:
         return self._has_cu_seqlens
 
+    def has_attention_mask(self) -> bool:
+        return self._has_attention_mask
+
     def get_batch_size(self) -> int:
         if self.batch_size is None:
             if self.has_cu_seqlens():
                 self.batch_size = self.cu_seqlens.size(0) - 1
-            elif self.attention_mask is not None:
+            elif self.has_attention_mask():
                 self.batch_size = self.attention_mask.size(0)
             else:
                 raise NotImplementedError(_ERROR_MESSAGE)
 
         return self.batch_size
 
-    def get_cu_seqlens(self) -> torch.Tensor | None:
+    def get_cu_seqlens(self, return_none_allowed: bool = True) -> torch.Tensor | None:
         if self.has_cu_seqlens():
             return self.cu_seqlens
 
-        if self.cu_seqlens is None:
-            if self.attention_mask is None:
-                B = self.get_batch_size()
-                S = self.get_max_seqlen()
+        if return_none_allowed:
+            return None
 
-                self.cu_seqlens = torch.arange(0, B * S + 1, S, dtype=torch.int32, device=self.device)
-            else:
-                seqlens = self.attention_mask.sum(dim=-1, dtype=torch.int32)
-                self.cu_seqlens = F.pad(torch.cumsum(seqlens, dim=0, dtype=torch.int32), (1, 0))
-                self.max_seqlen = seqlens.max().item()
+        if self.has_attention_mask():
+            seqlens = self.attention_mask.sum(dim=-1, dtype=torch.int32)
+            self.cu_seqlens = F.pad(torch.cumsum(seqlens, dim=0, dtype=torch.int32), (1, 0))
+            self.max_seqlen = seqlens.max().item()
+        else:
+            B = self.get_batch_size()
+            S = self.get_max_seqlen()
+
+            self.cu_seqlens = torch.arange(0, B * S + 1, S, dtype=torch.int32, device=self.device)
 
         return self.cu_seqlens
 
-    def get_max_seqlen(self) -> int | None:
+    def get_max_seqlen(self, return_none_allowed: bool = True) -> int | None:
         if self.has_cu_seqlens():
+            assert self.max_seqlen is not None
             return self.max_seqlen
 
+        if return_none_allowed:
+            return None
+
         if self.max_seqlen is None:
-            # this will cache the max_seqlen
+            # this will cache the max_seqlen but causes synchronization with CPU
             self.get_cu_seqlens()
 
             if self.max_seqlen is None:
@@ -98,32 +108,43 @@ class AttentionMaskInfo:
 
         return self.max_seqlen
 
-    def get_attention_mask(self) -> torch.Tensor | None:
-        if self.has_cu_seqlens() and self.attention_mask is None:
-            B = self.get_batch_size()
-            S = self.get_max_seqlen()
+    def get_attention_mask(self, return_none_allowed: bool = True) -> torch.Tensor | None:
+        if self.has_attention_mask():
+            return self.attention_mask
 
+        if return_none_allowed:
+            return None
+
+        B = self.get_batch_size()
+        S = self.get_max_seqlen()
+
+        if self.has_cu_seqlens():
             self.attention_mask = self.unpack_sequence(
                 inputs=torch.ones_like(self.get_cu_seqlens(), device=self.device, dtype=torch.int32),
                 output_shape=(B, S),
             )
+        else:
+            self.attention_mask = torch.ones(B, S, device=self.device, dtype=torch.int32)
 
         return self.attention_mask
 
     def get_position_ids(self) -> torch.Tensor:
-        if self.has_cu_seqlens():
-            attention_mask = self.get_attention_mask()
+        if self.has_cu_seqlens() or self.has_attention_mask():
+            attention_mask = self.get_attention_mask(False)
             position_ids = attention_mask.cumsum(-1)
         else:
-            position_ids = torch.arange(0, self.get_max_seqlen(), device=self.device)
-            position_ids = position_ids[None, ...].repeat(self.get_batch_size(), 1)
+            B = self.get_batch_size()
+            S = self.get_max_seqlen(False)
+
+            position_ids = torch.arange(0, S, device=self.device)
+            position_ids = position_ids[None, ...].repeat(B, 1)
 
         return position_ids
 
-    def get_causal_mask(self, query_length: int, dtype: torch.dtype) -> torch.Tensor | None:
-        attention_mask = self.get_attention_mask()
-
-        if attention_mask is None:
+    def get_causal_mask(self, query_length: int, return_none_allowed: bool = True) -> torch.Tensor | None:
+        if self.has_cu_seqlens() or self.has_attention_mask():
+            attention_mask = self.get_attention_mask()
+        elif return_none_allowed:
             return None
 
         Q = query_length
@@ -144,9 +165,9 @@ class AttentionMaskInfo:
         else:
             raise NotImplementedError(_ERROR_MESSAGE)
 
-        self._causal_mask = causal_mask[:, None, ...]
+        self.causal_mask = causal_mask[:, None, ...]
 
-        return self._causal_mask
+        return self.causal_mask
 
     def pack_sequence(self, inputs: torch.Tensor | list[torch.Tensor]) -> torch.Tensor | list[torch.Tensor]:
         if self.has_cu_seqlens():
@@ -187,10 +208,3 @@ class AttentionMaskInfo:
                 inputs = [i.reshape(B, S, *i.size()[1:]) for i in inputs]
 
         return inputs
-
-    @classmethod
-    def _get_mask_value(cls, device: torch.device, dtype: torch.dtype) -> torch.Tensor:
-        # torch.where expects a tensor. We use a cache to avoid recreating it every time.
-        if cls.mask_value is None or cls.mask_value.dtype != dtype or cls.mask_value.device != device:
-            cls.mask_value = torch.full([], torch.finfo(dtype).min, dtype=dtype, device=device)
-        return cls.mask_value
