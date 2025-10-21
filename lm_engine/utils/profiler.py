35852ecbfa6f8e767a9b59009f77b575f8b9cb6d
Mayank Mishra <mayank31398@gmail.com>
2025-10-21 16:22:37 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/utils/profiler.py
+++ lm_engine/utils/profiler.py
@@ -13,13 +13,13 @@ from .parallel import ProcessGroupManager
 class TorchProfiler:
     def __init__(self, path: str | None) -> TorchProfiler:
         if path is None:
-            self.torch_profiler = None
+            self._profiler = None
             return
 
         self.accelerator = Accelerator.get_accelerator()
 
         if self.accelerator == Accelerator.cuda:
-            self.torch_profiler = torch.profiler.profile(
+            self._profiler = torch.profiler.profile(
                 activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
                 schedule=torch.profiler.schedule(
                     wait=5 if ProcessGroupManager.get_global_rank() == 0 else 150000, warmup=5, active=1, repeat=1
@@ -29,7 +29,7 @@ class TorchProfiler:
                 profile_memory=True,
             )
         elif self.accelerator == Accelerator.tpu:
-            self.torch_profiler = torch.profiler.profile(
+            self._profiler = torch.profiler.profile(
                 activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
                 schedule=torch.profiler.schedule(
                     wait=5 if ProcessGroupManager.get_global_rank() == 0 else 150000, warmup=5, active=1, repeat=1
@@ -46,4 +46,5 @@ class TorchProfiler:
         return None
 
     def step(self) -> None:
-        self.torch_profiler.step()
+        if self._profiler is not None:
+            self._profiler.step()
