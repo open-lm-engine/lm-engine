f0eecf2bf17288d923964b1a5b7c3da040616c12
Mayank Mishra <mayank31398@gmail.com>
2025-10-22 02:43:55 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/training/params_group/params_group_test.py
+++ tests/training/params_group/params_group_test.py
@@ -5,10 +5,9 @@
 import json
 import os
 
-import torch
 from parameterized import parameterized
 
-from lm_engine.distributed import _get_parameter_marker_maps, _set_parameter_marker_maps
+from lm_engine.distributed import wrap_model_container_for_distributed_training
 from lm_engine.enums import ParamsGroupMethod
 from lm_engine.model_wrapper import get_model_container
 from lm_engine.optimization.params_group import get_param_groups_list
@@ -19,25 +18,28 @@ from ..test_commons import TestCommons
 
 class ParamsGroupTest(TestCommons):
     @parameterized.expand(
-        TestCommons.make_args_matrix([False, True], [("mup.json", ParamsGroupMethod.mup), ("normal.json", None)])
+        TestCommons.make_args_matrix(
+            [False, True], [False, True], [("mup.json", ParamsGroupMethod.mup), ("normal.json", None)]
+        )
     )
-    def test_mup_group(self, use_fsdp: bool, filename_method: tuple[str, ParamsGroupMethod | None]) -> None:
+    def test_mup_group(
+        self, use_fsdp: bool, use_torch_compile: bool, filename_method: tuple[str, ParamsGroupMethod | None]
+    ) -> None:
         expected_groups_filename, params_group_method = filename_method
         args = TestCommons.load_training_args_for_unit_tests("params_group/training_config.yml")
 
-        with (
-            torch.device("meta"),
-            ProcessGroupManager.set_dummy_tensor_parallel_world_size(1),
-            ProcessGroupManager.set_dummy_tensor_parallel_rank(0),
-            ProcessGroupManager.set_dummy_pipeline_parallel_world_size(1),
-            ProcessGroupManager.set_dummy_pipeline_parallel_rank(0),
-        ):
-            model_container = get_model_container(args, efficient_initialization=False, keep_in_fp32=False)
+        if not ProcessGroupManager.is_initialized():
+            os.environ["MASTER_ADDR"] = "localhost"
+            os.environ["MASTER_PORT"] = "29500"
+            os.environ["WORLD_SIZE"] = "1"
+            os.environ["RANK"] = "0"
 
-            if use_fsdp:
-                marker_maps = _get_parameter_marker_maps(model_container)
-                model_container = [torch.compile(model) for model in model_container]
-                _set_parameter_marker_maps(model_container, marker_maps)
+            ProcessGroupManager()
+
+        model_container = get_model_container(args, efficient_initialization=False, keep_in_fp32=False)
+
+        if use_fsdp or use_torch_compile:
+            wrap_model_container_for_distributed_training(args, model_container)
 
         params_groups = get_param_groups_list(model_container, args.optimizer_args.class_args, params_group_method)[0]
 
