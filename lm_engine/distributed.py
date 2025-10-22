31fd5ff7565a4970ebec8f95ca1e6765409b991c
Mayank Mishra <mayank31398@gmail.com>
2025-10-21 22:18:08 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/distributed.py
+++ lm_engine/distributed.py
@@ -221,6 +221,213 @@ def wrap_model_container_for_distributed_training(
 
     if accelerator == Accelerator.cuda:
         use_ddp = (stage == 0 or data_parallel_sharding_world_size == 1) and num_pipeline_stages == 1
+
+        mixed_precision_policy = _get_fsdp_mixed_precision(
+            dtype=dtype,
+            communication_dtype=communication_dtype,
+            fsdp_algorithm=2 if use_ddp else fsdp_algorithm,
+        )
+
+        if use_ddp or fsdp_algorithm == 2:
+            log_rank_0(logging.INFO, "using FSDP-2")
+            zero3 = stage == 3
+
+            def _sharding_function(parameter: nn.Parameter) -> Shard:
+                dps = (
+                    ProcessGroupManager.get_data_parallel_world_size()
+                    if data_parallel_sharding_world_size is None
+                    else data_parallel_sharding_world_size
+                )
+
+                if parameter.size(0) > dps or parameter.dim() == 1:
+                    return Shard(0)
+                else:
+                    for dim in range(1, parameter.dim()):
+                        if parameter.size(dim) > dps and parameter.size(dim) % dps == 0:
+                            return Shard(dim)
+
+                    log_rank_0(logging.WARN, "sharding along dim=0 since no suitable sharding dimension was found")
+                    return Shard(0)
+
+            for i, model in enumerate(model_container):
+                if efficient_initialization and model_name is not None:
+                    # state dict with Tensors
+                    old_state_dict = model.state_dict()
+
+                for module in model.modules():
+                    if isinstance(module, tuple(block_classes)):
+                        fully_shard(
+                            module,
+                            mesh=dp_mesh,
+                            reshard_after_forward=zero3,
+                            shard_placement_fn=_sharding_function,
+                            mp_policy=mixed_precision_policy,
+                            offload_policy=CPUOffloadPolicy(pin_memory=True) if cpu_offload else OffloadPolicy(),
+                        )
+
+                fully_shard(
+                    model,
+                    mesh=dp_mesh,
+                    reshard_after_forward=zero3,
+                    shard_placement_fn=None if use_ddp else _sharding_function,
+                    mp_policy=mixed_precision_policy,
+                    offload_policy=CPUOffloadPolicy(pin_memory=True) if cpu_offload else OffloadPolicy(),
+                )
+
+                if efficient_initialization:
+                    # contributed by Yu Chin Fabian Lim
+                    # original reference https://github.com/fabianlim/accelerate/pull/1
+                    if model_name is None:
+                        model = model.to_empty(device=torch.cuda.current_device())
+
+                        for module in model.modules():
+                            if hasattr(module, "reset_parameters"):
+                                with torch.device(torch.cuda.current_device()):
+                                    module.reset_parameters()
+                    else:
+                        if ProcessGroupManager.get_data_parallel_rank() == 0:
+                            model = model.to(torch.cuda.current_device())
+                        else:
+                            model = model.to_empty(device=torch.cuda.current_device())
+
+                            for module in model.modules():
+                                if hasattr(module, "reset_parameters"):
+                                    with torch.device(torch.cuda.current_device()):
+                                        module.reset_parameters()
+
+                        # state dict with DTensors
+                        new_state_dict = model.state_dict()
+
+                        for param_name, param in old_state_dict.items():
+                            if ProcessGroupManager.get_data_parallel_rank() == 0:
+                                full_tensor = param
+                            else:
+                                full_tensor = torch.empty(
+                                    param.shape, dtype=param.dtype, device=torch.cuda.current_device()
+                                )
+
+                            new_state_dict[param_name] = distribute_tensor(
+                                full_tensor,
+                                device_mesh=new_state_dict[param_name].device_mesh,
+                                placements=new_state_dict[param_name].placements,
+                            )
+
+                        model.load_state_dict(new_state_dict, assign=True)
+                        del old_state_dict, new_state_dict
+        elif fsdp_algorithm == 1:
+            log_rank_0(logging.INFO, "using FSDP-1")
+            assert num_pipeline_stages == 1
+
+            sharding_strategy = (
+                _STAGE_FULL_SHARDING_STRATEGY_MAP[stage]
+                if data_parallel_replication_world_size == 1
+                else _STAGE_HYBRID_SHARDING_STRATEGY_MAP[stage]
+            )
+
+            def _param_init(module: nn.Module) -> None:
+                assert len(teacher_block_names) == 0, "efficient initialization doesn't support distillation"
+
+                if model_name is None:
+                    module = module.to_empty(device=torch.cuda.current_device())
+
+                    if hasattr(module, "reset_parameters"):
+                        with torch.no_grad():
+                            module.reset_parameters()
+                else:
+                    if ProcessGroupManager.get_data_parallel_rank() != 0:
+                        module = module.to_empty(device=torch.cuda.current_device())
+
+            for i, model in enumerate(model_container):
+                model_container[i] = FSDP(
+                    model,
+                    compute_dtype=torch.bfloat16,
+                    buffer_dtype=torch.bfloat16,
+                    sharding_groups=[
+                        torch.distributed.get_process_group_ranks(ProcessGroupManager.get_data_parallel_group())
+                    ],
+                    sharding_rank=ProcessGroupManager.get_data_parallel_rank(),
+                    sharding_world_size=ProcessGroupManager.get_data_parallel_sharding_world_size(),
+                    auto_wrap_policy=partial(transformer_auto_wrap_policy, transformer_layer_cls=block_classes),
+                    param_init_fn=_param_init if efficient_initialization else None,
+                )
+        else:
+            raise ValueError(f"unexpected fsdp_algorithm ({fsdp_algorithm})")
+
+        if torch_compile:
+            log_rank_0(logging.INFO, "using torch compile")
+
+            for i, model in enumerate(model_container):
+                model_container[i] = torch.compile(model)
+
+        # _set_parameter_marker_maps(model_container, marker_maps)
+
+        pipeline_stages = []
+        pipeline_schedule = None
+
+        if num_pipeline_stages > 1:
+            micro_batch_size = args.training_parameters.micro_batch_size
+            sequence_length = args.datasets[0].class_args.get("sequence_length")
+
+            pipeline_parallel_schedule = args.distributed_args.pipeline_parallel_schedule
+            gradient_accumulation_steps = args.training_parameters.gradient_accumulation_steps
+
+            if pipeline_parallel_schedule == "1F1B":
+                assert (
+                    gradient_accumulation_steps % num_pipeline_stages == 0
+                ), f"gradient_accumulation_steps ({gradient_accumulation_steps}) should be divisible by num_pipeline_stages ({num_pipeline_stages})"
+
+            for model in model_container:
+                intermediate_dtype = string_to_torch_dtype(args.mixed_precision_args.dtype)
+
+                dummy_input_tensor = model.model.get_dummy_input_tensor(
+                    micro_batch_size, sequence_length, intermediate_dtype=intermediate_dtype
+                )
+                dummy_output_tensor = model.model.get_dummy_output_tensor(
+                    micro_batch_size,
+                    sequence_length,
+                    intermediate_dtype=intermediate_dtype,
+                    output_parallel_lm_logits_if_possible=True,
+                )
+
+                stage = PipelineStage(
+                    model,
+                    stage_index=model.pipeline_stage_id,
+                    num_stages=num_pipeline_stages,
+                    device=torch.cuda.current_device(),
+                    input_args=dummy_input_tensor,
+                    output_args=dummy_output_tensor,
+                    group=ProcessGroupManager.get_pipeline_parallel_group(),
+                )
+
+                pipeline_stages.append(stage)
+
+            lm_loss_multiplier = 1 / (
+                args.training_parameters.micro_batch_size * args.datasets[0].class_args.get("sequence_length")
+            )
+
+            def _pipeline_parallel_loss(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+                use_fused_linear_cross_entropy = is_kernel_allowed(Kernel.fused_linear_cross_entropy)
+
+                if isinstance(input, tuple):
+                    input, aux_loss = input
+                else:
+                    aux_loss = 0
+
+                output = CausalLMOutputWithPast(
+                    logits=None if use_fused_linear_cross_entropy else input,
+                    aux_loss=aux_loss,
+                    last_hidden_state=input if use_fused_linear_cross_entropy else None,
+                )
+                loss = model.get_loss(output, target, lm_loss_multiplier)
+
+                return loss
+
+            pipeline_schedule = _get_pipeline_parallel_schedule(
+                pipeline_parallel_schedule=args.distributed_args.pipeline_parallel_schedule,
+                gradient_accumulation_steps=args.training_parameters.gradient_accumulation_steps,
+                pipeline_stages=pipeline_stages,
+                loss_fn=_pipeline_parallel_loss,
+            )
     elif accelerator == Accelerator.tpu:
         assert (
             ProcessGroupManager.get_data_parallel_world_size()
@@ -230,212 +437,5 @@ def wrap_model_container_for_distributed_training(
         use_ddp = stage == 0 and num_pipeline_stages == 1
     else:
         raise ValueError(f"unexpected accelerator ({accelerator})")
-
-    mixed_precision_policy = _get_fsdp_mixed_precision(
-        dtype=dtype,
-        communication_dtype=communication_dtype,
-        fsdp_algorithm=2 if use_ddp else fsdp_algorithm,
-    )
-
-    if use_ddp or fsdp_algorithm == 2:
-        log_rank_0(logging.INFO, "using FSDP-2")
-        zero3 = stage == 3
-
-        def _sharding_function(parameter: nn.Parameter) -> Shard:
-            dps = (
-                ProcessGroupManager.get_data_parallel_world_size()
-                if data_parallel_sharding_world_size is None
-                else data_parallel_sharding_world_size
-            )
-
-            if parameter.size(0) > dps or parameter.dim() == 1:
-                return Shard(0)
-            else:
-                for dim in range(1, parameter.dim()):
-                    if parameter.size(dim) > dps and parameter.size(dim) % dps == 0:
-                        return Shard(dim)
-
-                log_rank_0(logging.WARN, "sharding along dim=0 since no suitable sharding dimension was found")
-                return Shard(0)
-
-        for i, model in enumerate(model_container):
-            if efficient_initialization and model_name is not None:
-                # state dict with Tensors
-                old_state_dict = model.state_dict()
-
-            for module in model.modules():
-                if isinstance(module, tuple(block_classes)):
-                    fully_shard(
-                        module,
-                        mesh=dp_mesh,
-                        reshard_after_forward=zero3,
-                        shard_placement_fn=_sharding_function,
-                        mp_policy=mixed_precision_policy,
-                        offload_policy=CPUOffloadPolicy(pin_memory=True) if cpu_offload else OffloadPolicy(),
-                    )
-
-            fully_shard(
-                model,
-                mesh=dp_mesh,
-                reshard_after_forward=zero3,
-                shard_placement_fn=None if use_ddp else _sharding_function,
-                mp_policy=mixed_precision_policy,
-                offload_policy=CPUOffloadPolicy(pin_memory=True) if cpu_offload else OffloadPolicy(),
-            )
-
-            if efficient_initialization:
-                # contributed by Yu Chin Fabian Lim
-                # original reference https://github.com/fabianlim/accelerate/pull/1
-                if model_name is None:
-                    model = model.to_empty(device=torch.cuda.current_device())
-
-                    for module in model.modules():
-                        if hasattr(module, "reset_parameters"):
-                            with torch.device(torch.cuda.current_device()):
-                                module.reset_parameters()
-                else:
-                    if ProcessGroupManager.get_data_parallel_rank() == 0:
-                        model = model.to(torch.cuda.current_device())
-                    else:
-                        model = model.to_empty(device=torch.cuda.current_device())
-
-                        for module in model.modules():
-                            if hasattr(module, "reset_parameters"):
-                                with torch.device(torch.cuda.current_device()):
-                                    module.reset_parameters()
-
-                    # state dict with DTensors
-                    new_state_dict = model.state_dict()
-
-                    for param_name, param in old_state_dict.items():
-                        if ProcessGroupManager.get_data_parallel_rank() == 0:
-                            full_tensor = param
-                        else:
-                            full_tensor = torch.empty(
-                                param.shape, dtype=param.dtype, device=torch.cuda.current_device()
-                            )
-
-                        new_state_dict[param_name] = distribute_tensor(
-                            full_tensor,
-                            device_mesh=new_state_dict[param_name].device_mesh,
-                            placements=new_state_dict[param_name].placements,
-                        )
-
-                    model.load_state_dict(new_state_dict, assign=True)
-                    del old_state_dict, new_state_dict
-    elif fsdp_algorithm == 1:
-        log_rank_0(logging.INFO, "using FSDP-1")
-        assert num_pipeline_stages == 1
-
-        sharding_strategy = (
-            _STAGE_FULL_SHARDING_STRATEGY_MAP[stage]
-            if data_parallel_replication_world_size == 1
-            else _STAGE_HYBRID_SHARDING_STRATEGY_MAP[stage]
-        )
-
-        def _param_init(module: nn.Module) -> None:
-            assert len(teacher_block_names) == 0, "efficient initialization doesn't support distillation"
-
-            if model_name is None:
-                module = module.to_empty(device=torch.cuda.current_device())
-
-                if hasattr(module, "reset_parameters"):
-                    with torch.no_grad():
-                        module.reset_parameters()
-            else:
-                if ProcessGroupManager.get_data_parallel_rank() != 0:
-                    module = module.to_empty(device=torch.cuda.current_device())
-
-        for i, model in enumerate(model_container):
-            model_container[i] = FSDP(
-                model,
-                compute_dtype=torch.bfloat16,
-                buffer_dtype=torch.bfloat16,
-                sharding_groups=[
-                    torch.distributed.get_process_group_ranks(ProcessGroupManager.get_data_parallel_group())
-                ],
-                sharding_rank=ProcessGroupManager.get_data_parallel_rank(),
-                sharding_world_size=ProcessGroupManager.get_data_parallel_sharding_world_size(),
-                auto_wrap_policy=partial(transformer_auto_wrap_policy, transformer_layer_cls=block_classes),
-                param_init_fn=_param_init if efficient_initialization else None,
-            )
-    else:
-        raise ValueError(f"unexpected fsdp_algorithm ({fsdp_algorithm})")
-
-    if torch_compile:
-        log_rank_0(logging.INFO, "using torch compile")
-
-        for i, model in enumerate(model_container):
-            model_container[i] = torch.compile(model)
-
-    # _set_parameter_marker_maps(model_container, marker_maps)
-
-    pipeline_stages = []
-    pipeline_schedule = None
-
-    if num_pipeline_stages > 1:
-        micro_batch_size = args.training_parameters.micro_batch_size
-        sequence_length = args.datasets[0].class_args.get("sequence_length")
-
-        pipeline_parallel_schedule = args.distributed_args.pipeline_parallel_schedule
-        gradient_accumulation_steps = args.training_parameters.gradient_accumulation_steps
-
-        if pipeline_parallel_schedule == "1F1B":
-            assert (
-                gradient_accumulation_steps % num_pipeline_stages == 0
-            ), f"gradient_accumulation_steps ({gradient_accumulation_steps}) should be divisible by num_pipeline_stages ({num_pipeline_stages})"
-
-        for model in model_container:
-            intermediate_dtype = string_to_torch_dtype(args.mixed_precision_args.dtype)
-
-            dummy_input_tensor = model.model.get_dummy_input_tensor(
-                micro_batch_size, sequence_length, intermediate_dtype=intermediate_dtype
-            )
-            dummy_output_tensor = model.model.get_dummy_output_tensor(
-                micro_batch_size,
-                sequence_length,
-                intermediate_dtype=intermediate_dtype,
-                output_parallel_lm_logits_if_possible=True,
-            )
-
-            stage = PipelineStage(
-                model,
-                stage_index=model.pipeline_stage_id,
-                num_stages=num_pipeline_stages,
-                device=torch.cuda.current_device(),
-                input_args=dummy_input_tensor,
-                output_args=dummy_output_tensor,
-                group=ProcessGroupManager.get_pipeline_parallel_group(),
-            )
-
-            pipeline_stages.append(stage)
-
-        lm_loss_multiplier = 1 / (
-            args.training_parameters.micro_batch_size * args.datasets[0].class_args.get("sequence_length")
-        )
-
-        def _pipeline_parallel_loss(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-            use_fused_linear_cross_entropy = is_kernel_allowed(Kernel.fused_linear_cross_entropy)
-
-            if isinstance(input, tuple):
-                input, aux_loss = input
-            else:
-                aux_loss = 0
-
-            output = CausalLMOutputWithPast(
-                logits=None if use_fused_linear_cross_entropy else input,
-                aux_loss=aux_loss,
-                last_hidden_state=input if use_fused_linear_cross_entropy else None,
-            )
-            loss = model.get_loss(output, target, lm_loss_multiplier)
-
-            return loss
-
-        pipeline_schedule = _get_pipeline_parallel_schedule(
-            pipeline_parallel_schedule=args.distributed_args.pipeline_parallel_schedule,
-            gradient_accumulation_steps=args.training_parameters.gradient_accumulation_steps,
-            pipeline_stages=pipeline_stages,
-            loss_fn=_pipeline_parallel_loss,
-        )
 
     return model_container, pipeline_schedule
