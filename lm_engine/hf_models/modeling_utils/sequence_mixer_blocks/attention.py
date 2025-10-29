9c57fed8071ab1d4bb3788ca74059aa7b165c7b9
Mayank Mishra <mayank31398@gmail.com>
2025-10-29 13:53:02 -0700

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/attention.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/attention.py
@@ -14,11 +14,9 @@ from ....enums import Kernel
 from ....kernels import is_kernel_allowed, wait_for_ACT
 from ....utils import Accelerator, divide_if_divisible, is_torch_xla_available
 from ...cache import GenerationCache
-from ...modeling_utils import ParameterizedConv1d, get_activation_function, is_glu
 from ...parameter import mark_parameter_as_mup_learning_rate
 from ..linear import ParameterizedLinear
 from ..position_embedding import apply_rotary_pos_emb
-from .causal_convolution import causal_convolution
 from .utils import flash_attention
 
 
