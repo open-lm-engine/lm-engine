dc214099151b550a785687e56bb3f1e23c85989b
Mayank Mishra <mayank31398@gmail.com>
2025-10-30 16:54:00 -0700

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/utils/parallel.py
+++ lm_engine/utils/parallel.py
@@ -399,31 +399,16 @@ def run_rank_n(func: Callable, rank: int = 0, barrier: bool = False) -> Callable
 
     # wrapper function for the rank to execute on
     def func_rank_n(*args, **kwargs):
-        output = func(*args, **kwargs)
+        output = func(*args, **kwargs) if ProcessGroupManager.get_global_rank() == rank else None
+
         if barrier:
             from .communication import Communication
 
             Communication.barrier()
+
         return output
 
-    # a dummy method that doesn't do anything
-    def func_rank_other(*args, **kwargs):
-        if barrier:
-            from .communication import Communication
-
-            Communication.barrier()
-
-    global_rank = ProcessGroupManager.get_global_rank()
-
-    if global_rank == rank:
-        wrapped_func = func_rank_n
-    elif global_rank is None:
-        # distributed is not initialized
-        wrapped_func = func
-    else:
-        wrapped_func = func_rank_other
-
-    return wrapped_func
+    return func_rank_n
 
 
 def is_tracking_rank() -> bool:
