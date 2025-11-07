3c5a6fbb035a1955b99f20e7712585a375f79166
Mayank Mishra <mayank31398@gmail.com>
2025-11-06 16:44:39 -0800

    slight cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/data/megatron/dtype.py
+++ lm_engine/data/megatron/dtype.py
@@ -1,9 +1,6 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-# Essentially re-written in entirety
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
 
 from __future__ import annotations
 
--- lm_engine/data/megatron/indexed_dataset.py
+++ lm_engine/data/megatron/indexed_dataset.py
@@ -1,6 +1,9 @@
-# **************************************************
-# Copyright (c) 2025, Mayank Mishra
-# **************************************************
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Essentially re-written in entirety
 
 from __future__ import annotations
 
