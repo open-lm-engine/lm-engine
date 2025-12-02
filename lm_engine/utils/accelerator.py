6fc5b0ac3db782c7e010b12085a2af7ae91348da
Mayank Mishra <mayank31398@gmail.com>
2025-12-01 20:53:57 -0800

    chunk
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/utils/accelerator.py
+++ lm_engine/utils/accelerator.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 from enum import Enum
+from functools import lru_cache
 from typing import Any
 
 import torch
@@ -18,30 +19,35 @@ if is_torch_xla_available():
     from torch_xla.core.xla_model import xla_device
 
 
+_IS_ROCM_AVAILABLE = torch.version.hip is not None
+
+
 class Accelerator(Enum):
     cpu = "cpu"
     cuda = "cuda"
+    rocm = "rocm"
     tpu = "tpu"
     trainium = "trainium"
 
     @staticmethod
+    @lru_cache
     def get_accelerator() -> Accelerator:
-        if torch.cuda.is_available():
-            accelerator = Accelerator.cuda
-        elif is_torch_xla_available():
+        if is_torch_xla_available():
             accelerator = Accelerator.tpu
         elif is_torch_neuronx_available():
             accelerator = Accelerator.trainium
+        elif torch.cuda.is_available():
+            accelerator = Accelerator.rocm if _IS_ROCM_AVAILABLE else Accelerator.cuda
         else:
             accelerator = Accelerator.cpu
 
         return accelerator
 
     @staticmethod
-    def get_current_device() -> int:
+    def get_current_device() -> int | str:
         accelerator = Accelerator.get_accelerator()
 
-        if accelerator == Accelerator.cuda:
+        if accelerator in [Accelerator.cuda, Accelerator.rocm]:
             device = torch.cuda.current_device()
         elif accelerator == Accelerator.tpu:
             device = xla_device()
@@ -53,10 +59,11 @@ class Accelerator(Enum):
         return device
 
     @staticmethod
+    @lru_cache
     def get_device_type() -> str:
         accelerator = Accelerator.get_accelerator()
 
-        if accelerator == Accelerator.cuda:
+        if accelerator in [Accelerator.cuda, Accelerator.rocm]:
             device = "cuda"
         elif accelerator == Accelerator.tpu:
             device = "xla"
@@ -80,7 +87,7 @@ class Accelerator(Enum):
     def get_rng_state() -> Any:
         accelerator = Accelerator.get_accelerator()
 
-        if accelerator == Accelerator.cuda:
+        if accelerator in [Accelerator.cuda, Accelerator.rocm]:
             state = torch.cuda.get_rng_state()
         elif accelerator == Accelerator.tpu:
             state = xla_get_rng_state()
@@ -95,7 +102,7 @@ class Accelerator(Enum):
     def set_rng_state(state: Any) -> Any:
         accelerator = Accelerator.get_accelerator()
 
-        if accelerator == Accelerator.cuda:
+        if accelerator in [Accelerator.cuda, Accelerator.rocm]:
             state = torch.cuda.set_rng_state(state)
         elif accelerator == Accelerator.tpu:
             state = xla_set_rng_state(state)
