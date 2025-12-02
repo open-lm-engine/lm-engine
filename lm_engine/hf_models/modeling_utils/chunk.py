6e017650316ae3c1998b7ab9320dd1963ee1f5c9
Mayank Mishra <mayank31398@gmail.com>
2025-12-01 17:54:49 -0800

    chunk
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/chunk.py
+++ lm_engine/hf_models/modeling_utils/chunk.py
@@ -19,3 +19,19 @@ class _ContiguousChunk(torch.autograd.Function):
 
 def contiguous_chunk(x: torch.Tensor, chunks: int, dim: int = 0) -> tuple[torch.Tensor]:
     return _ContiguousChunk.apply(x, chunks, dim)
+
+
+class _ContiguousSplit(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, split_size: int, dim: int) -> torch.Tensor:
+        ctx.dim = dim
+        x = x.split(split_size, dim=dim)
+        return tuple(i.contiguous() for i in x)
+
+    @staticmethod
+    def backward(ctx, *dy: tuple[torch.Tensor]) -> tuple[torch.Tensor, None, None]:
+        return torch.cat(dy, dim=ctx.dim), None, None
+
+
+def contiguous_split(x: torch.Tensor, split_size: tuple[int, ...], dim: int = 0) -> tuple[torch.Tensor]:
+    return _ContiguousSplit.apply(x, split_size, dim)
