d2c4ef737e740b92fd33980ff6a407fcd901b7e1
Mayank Mishra <mayank31398@gmail.com>
2025-12-06 23:08:00 -0800

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -1,5 +1,5 @@
 INPUT_PATH=/data/tmp/test/part_000045.parquet
-OUTPUT_PATH=/data/tmp/test-tokenized/part_000045_text
+OUTPUT_PATH=/data/tmp/test-tokenized/part_000045
 TOKENIZER=ibm-granite/granite-4.0-h-tiny-base
 
 ray job submit --address http://localhost:8265 -- bash -c "rm -rf test && mkdir -p test && cd test && git clone --recurse-submodules https://github.com/open-lm-engine/lm-engine && source ../lm-engine/.venv/bin/activate && cd lm-engine && git checkout test && uv pip install multi-storage-client && python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 1 --download-locally"
