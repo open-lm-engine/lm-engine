55498c9bd0f04a947b24cf5ec6f1547bf4f2c932
Mayank Mishra <mayank31398@gmail.com>
2025-12-06 17:28:06 -0800

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -2,4 +2,4 @@ INPUT_PATH=data.jsonl
 OUTPUT_PATH=data
 TOKENIZER=ibm-granite/granite-3b-code-base
 
-ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && source .venv/bin/activate && python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod"
+ray job submit --address http://localhost:8265 -- bash -c "source .venv/bin/activate && cd lm-engine && python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod"
