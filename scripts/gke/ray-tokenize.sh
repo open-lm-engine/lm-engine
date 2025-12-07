73d4f52040bc513fb70c43e86a09844f5d9f50f8
Mayank Mishra <mayank31398@gmail.com>
2025-12-06 22:18:15 -0800

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -1,5 +1,5 @@
-INPUT_PATH=/data/tmp/Nemotron-CC-v2/Translated-Diverse-QA/
-OUTPUT_PATH=/data/tmp/Nemotron-CC-v2-tokenized/Translated-Diverse-QA/
+INPUT_PATH=/data/tmp/test/
+OUTPUT_PATH=/data/tmp/test-tokenized/
 TOKENIZER=ibm-granite/granite-4.0-h-tiny-base
 
 ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && source .venv/bin/activate && python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 1"
