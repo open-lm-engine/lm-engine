9605ddf0c4a74b0f9ab466425790be4974511043
Mayank Mishra <mayank31398@gmail.com>
2025-12-08 11:49:47 -0800

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
-docker build -f docker/Dockerfile --platform linux/amd64 --build-arg -t ghcr.io/open-lm-engine/data:latest .
+docker build -f docker/data.Dockerfile --platform linux/amd64 -t ghcr.io/open-lm-engine/data:latest .
