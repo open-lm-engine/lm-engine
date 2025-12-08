ef77f8ee437c8442f6a0192c723c34e78b48761d
Mayank Mishra <mayank31398@gmail.com>
2025-12-08 11:39:56 -0800

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- a.py
+++
@@ -1,4 +0,0 @@
-import multistorageclient as msc
-
-
-print(list(msc.list(f"msc://mayank-data/tmp/test/")))
--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -2,6 +2,6 @@ INPUT_PATH=/data/tmp/Nemotron-CC-v2
 OUTPUT_PATH=/data/tmp/Nemotron-CC-v2-tokenized
 TOKENIZER=ibm-granite/granite-4.0-h-tiny-base
 
-ray job submit --address http://localhost:8265 -- bash -c "rm -rf lm-engine && git clone --recurse-submodules https://github.com/open-lm-engine/lm-engine && cd lm-engine && git checkout test && uv pip install -e . && MSC_CONFIG=/app/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 32 --download-locally --msc-base-path mayank-data"
+# ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && MSC_CONFIG=/app/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 16 --download-locally --msc-base-path mayank-data"
 
-# ray job stop --address  http://localhost:8265 02000000
+ray job stop --address  http://localhost:8265 03000000
