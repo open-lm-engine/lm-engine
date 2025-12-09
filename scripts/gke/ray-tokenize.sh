076f54323c6e72860af19a72aa45f94cafe713df
Huanzhi Mao <huanzhimao@gmail.com>
2025-12-08 23:12:32 -0800

    1

--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -2,6 +2,6 @@ INPUT_PATH=/data/tmp/test/part_000045.parquet
 OUTPUT_PATH=/data/tmp/Nemotron-CC-v2-tokenized/test
 TOKENIZER=ibm-granite/granite-4.0-h-tiny-base
 
-ray job submit --address http://localhost:8265 -- bash -c "rm -rf lm-engine && git clone https://github.com/open-lm-engine/lm-engine.git -b test --recursive && cd lm-engine && uv pip install -e . && MSC_CONFIG=/app/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 1 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
+ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && git checkout test && git pull && uv pip install -e . && MSC_CONFIG=/app/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 1 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
 
 # ray job stop --address  http://localhost:8265 03000000
