25c90d4a89a51e283d427ea3e2d495f4726be7eb
Mayank Mishra <mayank31398@gmail.com>
2025-12-10 12:40:02 -0800

    reformat
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/data/megatron/preprocess_data.py
+++ lm_engine/data/megatron/preprocess_data.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import json
+import os
 import tempfile
 from typing import Iterator
 
@@ -73,6 +74,7 @@ def convert_file(
     subset: str | None = None,
     json_keys: list[str] = ["text"],
     append_eos_token: bool = True,
+    tmpdir: str | None = None,
 ) -> None:
     encoder = Encoder(tokenizer, json_keys, append_eos_token)
 
@@ -81,9 +83,10 @@ def convert_file(
         encoded_docs = map(encoder.encode, open(input_file, "r", encoding="utf-8"))
     elif input_file.endswith(".jsonl.zst"):
         assert subset is None, f"zst jsonl doesn't support a subset"
+        assert tmpdir is not None
 
         dctx = ZstdDecompressor()
-        outfile = tempfile.TemporaryFile(suffix=input_file.rstrip(".zst"))
+        outfile = tempfile.TemporaryFile(suffix=os.path.basename(input_file.rstrip(".zst")), dir=tmpdir)
         with open(input_file, "rb") as infile:
             dctx.copy_stream(infile, outfile)
         outfile.seek(0)
--- tools/data/preprocess_data.py
+++ tools/data/preprocess_data.py
@@ -96,6 +96,7 @@ def process_file_ray(args: Namespace, input_file: str, output_prefix: str) -> No
                     subset=args.subset,
                     json_keys=args.json_keys,
                     append_eos_token=args.append_eod,
+                    tmpdir=args.tmpdir,
                 )
 
                 for key in args.json_keys:
@@ -111,6 +112,7 @@ def process_file_ray(args: Namespace, input_file: str, output_prefix: str) -> No
                 subset=args.subset,
                 json_keys=args.json_keys,
                 append_eos_token=args.append_eod,
+                tmpdir=args.tmpdir,
             )
 
         return input_file
@@ -248,6 +250,7 @@ def main() -> None:
             subset=args.subset,
             json_keys=args.json_keys,
             append_eos_token=args.append_eod,
+            tmpdir=args.tmpdir,
         )
 
         log_rank_0(logging.INFO, "✅ File processed successfully.")
