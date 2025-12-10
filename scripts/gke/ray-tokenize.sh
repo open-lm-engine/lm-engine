0ede8b51b30bafda650a2a7e32e2761e803626ed
Huanzhi Mao <huanzhimao@gmail.com>
2025-12-09 21:27:56 -0800

    avoid ram usage

--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -2,6 +2,6 @@ INPUT_PATH=/data/tmp/Nemotron-CC-v2
 OUTPUT_PATH=/data/Nemotron-CC-v2
 TOKENIZER=ibm-granite/granite-4.0-h-tiny-base
 
-ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && git init && find . -name .git -type f -delete && git remote add origin https://github.com/open-lm-engine/lm-engine || true && git fetch origin && git reset --hard origin/test && uv pip install -e . && MSC_CONFIG=/app/lm-engine/configs/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 800 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
+ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && git init && find . -name .git -type f -delete && git remote add origin https://github.com/open-lm-engine/lm-engine || true && git fetch origin && git reset --hard origin/test && uv pip install -e . && MSC_CONFIG=/app/lm-engine/configs/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 700 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
 
 # ray job stop --address  http://localhost:8265 03000000
