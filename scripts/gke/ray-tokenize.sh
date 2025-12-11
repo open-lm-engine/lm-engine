fcaad8193c52099d4e981bf5c79cf8d08fb7fc9c
Huanzhi Mao <huanzhimao@gmail.com>
2025-12-11 00:24:14 -0800

    clone in worker setup

--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -1,9 +1,10 @@
-INPUT_PATH=/data/tmp/dolma3-pool
+# INPUT_PATH=/data/tmp/dolma3-pool
+INPUT_PATH=/data/tmp/dolma3-pool/data/common_crawl-education_and_jobs-0014/shard_00000000.jsonl.zst
 OUTPUT_PATH=/data/tmp/dolma3-pool-tokenized
 # INPUT_PATH=/data/tmp/test
 # OUTPUT_PATH=/data/tmp/test-tokenized
 TOKENIZER=ibm-granite/granite-4.0-h-tiny-base
 
-ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && git init && find . -name .git -type f -delete && git remote add origin https://github.com/open-lm-engine/lm-engine || true && git fetch origin && git reset --hard origin/test && uv pip install -e . && MSC_CONFIG=/app/lm-engine/configs/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 2000 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
+ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && uv pip install -e . && MSC_CONFIG=/app/lm-engine/configs/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 2000 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
 
 # ray job stop --address  http://localhost:8265 03000000
