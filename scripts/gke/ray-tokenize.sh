ac08b05c09072e55806457d135456598449f04c6
Huanzhi Mao <huanzhimao@gmail.com>
2025-12-13 01:20:37 -0800

    1

--- scripts/gke/ray-tokenize.sh
+++ scripts/gke/ray-tokenize.sh
@@ -1,5 +1,5 @@
-# INPUT_PATHS=(
-# "olmocr_science_pdfs-adult_content"
+INPUT_PATHS=(
+"olmocr_science_pdfs-adult_content"
 # "olmocr_science_pdfs-art_and_design"
 # "olmocr_science_pdfs-crime_and_law"
 # "olmocr_science_pdfs-education_and_jobs"
@@ -23,23 +23,23 @@
 # "olmocr_science_pdfs-sports_and_fitness"
 # "olmocr_science_pdfs-transportation"
 # "olmocr_science_pdfs-travel_and_tourism"
-# )
-
-
-INPUT_PATHS=(
-"finemath-3plus"
-"finemath-4plus"
-"infiwebmath-3plus"
-"infiwebmath-4plus"
 )
+
+
+# INPUT_PATHS=(
+# "finemath-3plus"
+# "finemath-4plus"
+# "infiwebmath-3plus"
+# "infiwebmath-4plus"
+# )
 
 TOKENIZER=/app/lm-engine/tokenizers/granite-4.0-h-tiny-base
 
 for ITEM in "${INPUT_PATHS[@]}"; do
-    INPUT_PATH="/data/tmp/finemath/$ITEM"
-    OUTPUT_PATH="/data/tmp/finemath-tokenized/$ITEM"
+    INPUT_PATH="/data/tmp3/dolma3-pool/data/$ITEM"
+    OUTPUT_PATH="/data/tmp3/dolma3-pool-tokenized/data/$ITEM"
     
-    ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && uv pip install -e . && MSC_CONFIG=/app/lm-engine/configs/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 20000 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
+    ray job submit --address http://localhost:8265 -- bash -c "cd lm-engine && git fetch && git reset --hard origin/test && uv pip install -e . && MSC_CONFIG=/app/lm-engine/configs/msc/gcs.yml python tools/data/preprocess_data.py --input $INPUT_PATH --tokenizer $TOKENIZER --output-prefix $OUTPUT_PATH --append-eod --ray-workers 20000 --download-locally --msc-base-path mayank-data --tmpdir /local-ssd"
 done
 
 # ray job stop --address  http://localhost:8265 03000000
