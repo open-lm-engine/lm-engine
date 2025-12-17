41ec9be41d9226e08a0fae2ca923b1bcee04a641
Mayank Mishra <mayank31398@gmail.com>
2025-12-16 20:22:24 -0800

    test

--- lm_engine/checkpointing/__init__.py
+++ lm_engine/checkpointing/__init__.py
@@ -128,7 +128,11 @@ def save_checkpoint(
                 "Therefore, the function will not save the optimizer",
             )
 
+    accelerator = Accelerator.get_accelerator()
+
     if args.save_args.async_checkpointing:
+        assert accelerator == Accelerator.cuda
+
         global _FUTURE
         _FUTURE = dcp.async_save(
             {
@@ -154,8 +158,6 @@ def save_checkpoint(
 
         _FUTURE.add_done_callback(_f)
     else:
-        accelerator = Accelerator.get_accelerator()
-
         if accelerator == Accelerator.cuda:
             dcp.save({"state": _ModelSaver(model_container)}, checkpoint_id=_get_model_path(save_path))
 
