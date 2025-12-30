0eb29eef2a6cb18fd7ab136d1c47f5d19d341b83
Mayank Mishra <mayank31398@gmail.com>
2025-12-29 20:31:33 -0800

    init

--- tools/data/merge_data.py
+++ tools/data/merge_data.py
@@ -32,42 +32,36 @@ def get_args() -> Namespace:
     parser.add_argument("--workers", type=int, default=1, required=False, help="number of workers")
 
     parser.add_argument(
-        "--input-directory",
-        type=str,
-        required=False,
-        help="Path to directory containing all document files to merge",
+        "--input-directory", type=str, required=False, help="Path to directory containing all document files to merge"
     )
 
-    parser.add_argument(
-        "--output-prefix",
-        type=str,
-        required=True,
-        help="Path to binary output file without suffix",
-    )
+    parser.add_argument("--output-prefix", type=str, required=True, help="Path to binary output file without suffix")
     parser.add_argument("--max-size", type=int, required=False, help="max file size")
-    parser.add_argument(
-        "--specific-group",
-        type=int,
-        required=False,
-        help="Run only specific group index",
-    )
+    parser.add_argument("--specific-group", type=int, required=False, help="Run only specific group index")
 
     parser.add_argument("--download-locally", action="store_true", help="download file locally")
     parser.add_argument("--msc-base-path", type=str, help="base path for MSC")
     parser.add_argument("--tmpdir", type=str, help="temporary local directory")
 
+    parser.add_argument("--ray-job", action="store_true", help="whether to use ray")
+
     args = parser.parse_args()
     assert args.input_prefixes is not None or args.input_directory is not None
 
     if args.download_locally:
+        assert args.ray_job
         assert args.msc_base_path
         assert args.tmpdir
+
+    if args.msc_base_path:
+        assert args.ray_job
 
     return args
 
 
 def _convert_path_to_msc_path_and_tmp_path(path: str, base_msc_path: str, tmpdir: str) -> tuple[str, str]:
     path = path.lstrip(os.sep)
+
     try:
         _, base_path = path.split(os.sep, 1)
     except ValueError:
@@ -76,6 +70,7 @@ def _convert_path_to_msc_path_and_tmp_path(path: str, base_msc_path: str, tmpdir
     path = os.path.join(base_msc_path, base_path)
     path = f"{MSC_PREFIX}{path}"
     local_path = os.path.join(tmpdir, base_path)
+
     return path, local_path
 
 
@@ -140,55 +135,6 @@ def merge_files_remote(subdir: str, input_prefixes: list[str], output_prefix: st
         print(f"Error processing {output_prefix}: {e}")
         traceback.print_exc()
         raise e
-
-
-# def get_groups_by_sizes(path: str, max_size: int | None = None) -> list[list[str]]:
-#     # Expand path to include sibling directories with same prefix
-#     path = path.rstrip(os.sep)
-#     base_dir = os.path.dirname(path)
-#     # if not base_dir:
-#     #     base_dir = "."
-#     search_prefix = os.path.basename(path)
-
-#     input_dirs = []
-#     if os.path.isdir(base_dir):
-#         # We need to list directories in base_dir
-#         for d in sorted(os.listdir(base_dir)):
-#             full_path = os.path.join(base_dir, d)
-#             if os.path.isdir(full_path) and d.startswith(search_prefix):
-#                 input_dirs.append(full_path)
-#     if not input_dirs:
-#         input_dirs = [path]
-
-#     print(f"Merging content from directories: {input_dirs}")
-
-#     fnames = []
-#     for d in input_dirs:
-#         if os.path.isdir(d):
-#             curr_fnames = filter(lambda x: x.endswith(".bin"), os.listdir(d))
-#             curr_fnames = [os.path.join(d, i) for i in curr_fnames]
-#             fnames.extend(curr_fnames)
-
-#     fnames = [i[:-4] for i in fnames]
-
-#     if max_size is None:
-#         return [fnames]
-
-#     max_size *= 1024**3
-#     groups = []
-#     current_grp = []
-#     current_size = 0
-
-#     for index, fname in enumerate(tqdm(fnames, desc="Grouping files")):
-#         current_grp.append(fname)
-#         current_size += os.path.getsize(f"{fname}.bin")
-
-#         if current_size > max_size or index == len(fnames) - 1:
-#             groups.append(current_grp)
-#             current_grp = []
-#             current_size = 0
-
-#     return groups
 
 
 def get_groups_by_sizes(path: str, max_size: int | None = None) -> list[list[str]]:
@@ -266,9 +212,6 @@ if __name__ == "__main__":
         file_groups = get_groups_by_sizes(args.input_directory, args.max_size)
         if args.max_size is None:
             raise ValueError("Max size is required")
-            # merge_files_wrapper(
-            #     input_prefixes=file_groups[0][1], output_prefix=os.path.join(args.output_prefix, file_groups[0][0]), args=args
-            # )
         else:
             ray.init(
                 address="auto",
