01ebc8af115603116ba6db65a55e3c65dafe2756
Mayank Mishra <mayank31398@gmail.com>
2026-01-05 21:32:40 -0800

    do_sigmoid_gate argument
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/rsa.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/rsa.py
@@ -181,7 +181,7 @@ class RSA(nn.Module):
             gradient_clipping=self.gradient_clipping,
             cu_seqlens=cu_seqlens,
             max_seqlen=max_seqlen,
-            do_gate_sigmoid=self.use_softplus_decay,
+            do_gate_sigmoid=not self.use_softplus_decay,
         )
 
         if self.use_residual:
