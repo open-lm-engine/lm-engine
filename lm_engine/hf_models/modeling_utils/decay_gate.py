7badbce4881e90b640d1de8859c4c0705b7e25ed
Mayank Mishra <mayank31398@gmail.com>
2026-02-04 11:32:22 -0800

    fix RNN init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/decay_gate.py
+++ lm_engine/hf_models/modeling_utils/decay_gate.py
@@ -57,7 +57,7 @@ class SoftplusDecayGate(nn.Module):
 
     @torch.no_grad()
     def reset_parameters(self) -> None:
-        A = 16 * torch.rand(self.output_size, dtype=torch.float32)
+        A = torch.empty(self.output_size, dtype=torch.float32).uniform_(1, 16)
         self.A_log.copy_(torch.log(A))
 
         dt_min = 0.001
