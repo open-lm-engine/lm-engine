323ad5a577093b3bf8812e5dcd562582ef4e6ef1
Mayank Mishra <mayank31398@gmail.com>
2026-02-09 13:30:42 -0800

    fix linter
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils_TP/__init__.py
+++ lm_engine/hf_models/modeling_utils_TP/__init__.py
@@ -3,5 +3,5 @@
 # **************************************************
 
 from .lm_head import LMHead_TP
-from .mlp_blocks import MoE_TP, get_mlp_block_TP
+from .mlp_blocks import get_mlp_block_TP
 from .sequence_mixer_blocks import Attention_TP, get_sequence_mixer_TP
