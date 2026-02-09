ed561e819e168ec0c441890d11a81c9a7882a5dc
Mayank Mishra <mayank31398@gmail.com>
2026-02-09 02:18:00 -0800

    norm
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils_TP/__init__.py
+++ lm_engine/hf_models/modeling_utils_TP/__init__.py
@@ -3,6 +3,5 @@
 # **************************************************
 
 from .lm_head import LMHead_TP
-from .mlp_blocks import MLP_TP, MoE_TP, get_mlp_block_TP
+from .mlp_blocks import MoE_TP, get_mlp_block_TP
 from .sequence_mixer_blocks import Attention_TP, get_sequence_mixer_TP
-from .TP import get_module_placements, tensor_parallel_split_safetensor_slice
