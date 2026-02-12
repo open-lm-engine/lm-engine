1bbfd3116d74b0e51b334fcdab8f70b6758fb137
Mayank Mishra <mayank31398@gmail.com>
2026-02-12 15:21:10 -0800

    concise
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/mixins/dense/main.py
+++ lm_engine/hf_models/mixins/dense/main.py
@@ -51,8 +51,6 @@ class CausalLMModelMixin(PreTrainedModelMixin, DTensorModule):
 
             self.m_width = config.m_width
 
-        self.tp_mesh = ProcessGroupManager.get_tensor_parallel_mesh() if self.is_tp_enabled else None
-
     def forward(
         self,
         input_ids: torch.Tensor | list[list[int]] | None = None,
