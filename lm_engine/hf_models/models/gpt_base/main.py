04e20df501397ae9adbb3f3b043f16fac89595db
Mayank Mishra <mayank31398@gmail.com>
2026-02-14 15:00:02 -0800

    concise
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/models/gpt_base/main.py
+++ lm_engine/hf_models/models/gpt_base/main.py
@@ -4,7 +4,9 @@
 
 from ...mixins import CausalLMModelMixin
 from .base import GPTBaseModel, GPTBasePreTrainedModel
+from .weights import get_gpt_base_model_parallel_state_dict
 
 
 class GPTBaseForCausalLM(GPTBasePreTrainedModel, CausalLMModelMixin):
     base_model_class = GPTBaseModel
+    model_parallel_state_dict_function = get_gpt_base_model_parallel_state_dict
--- lm_engine/hf_models/models/gpt_base_TP/__init__.py
+++
@@ -1,5 +0,0 @@
-# **************************************************
-# Copyright (c) 2025, Mayank Mishra
-# **************************************************
-
-from .weights import fix_gpt_base_unsharded_state_dict, unshard_gpt_base_tensor_parallel_state_dicts
