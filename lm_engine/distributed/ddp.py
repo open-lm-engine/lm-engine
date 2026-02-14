6fd4d010480a591e7a32c52c2dfa524f4a339e51
Mayank Mishra <mayank31398@gmail.com>
2026-02-14 00:05:16 -0800

    ddp
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/distributed/ddp.py
+++ lm_engine/distributed/ddp.py
@@ -8,25 +8,22 @@ from typing import Any
 
 import torch
 import torch.nn as nn
-from torch.distributed import DeviceMesh, ReduceOp
-
-from ..utils import ProcessGroupManager
+from torch.distributed import ProcessGroup, ReduceOp
 
 
 class DDP(nn.Module):
     def __init__(
         self,
         model: nn.Module,
-        device_mesh: DeviceMesh | None = None,
+        process_group: ProcessGroup,
         sync_module_states: bool = False,
         overlap_communication: bool = False,
     ) -> DDP:
         super().__init__()
 
         self.model = model
-        self.device_mesh = ProcessGroupManager.get_data_parallel_mesh() if device_mesh is None else device_mesh
-        self.process_group = self.device_mesh.get_group()
         self.overlap_communication = overlap_communication
+        self.process_group = process_group
 
         if self.overlap_communication:
             raise NotImplementedError()
