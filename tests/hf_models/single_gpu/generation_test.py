f300e55e258e44d11953a262a9f3ecd8e847884e
Mayank Mishra <mayank31398@gmail.com>
2026-03-23 19:38:29 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/single_gpu/generation_test.py
+++ tests/hf_models/single_gpu/generation_test.py
@@ -3,29 +3,26 @@
 # **************************************************
 
 
+import pytest
 import torch
-from parameterized import parameterized
 
-from ..test_common import TestCommons
+from ...test_common import skip_test_if_device_unavailable
+from ..test_common import from_config, get_dense_test_config, get_dummy_inputs, get_moe_test_config
 
 
-class GenerationTest(TestCommons):
-    @parameterized.expand(
-        TestCommons.make_args_matrix(
-            TestCommons.get_all_devices(), TestCommons.get_position_embedding_types(), TestCommons.get_dtypes()
-        )
-    )
-    def test_generation_works(self, device: torch.device, position_embedding_type: str, dtype: torch.dtype) -> None:
-        self.skip_test_if_device_unavailable(device)
-        self.skip_test_if_layernorm_kernel_unavailable(device, dtype)
+def test_generation_works(device: torch.device, position_embedding_type: str, dtype: torch.dtype) -> None:
+    skip_test_if_device_unavailable(device)
 
-        for config in [
-            self.get_dense_test_config(position_embedding_type),
-            self.get_moe_test_config(position_embedding_type),
-        ]:
-            model = self.from_config(config, dtype=dtype).to(device)
-            model.eval()
+    if device.type == "cpu" and dtype == torch.float16:
+        pytest.skip("LayerNormKernelImpl not implemented for Half")
 
-            input_ids, attention_mask, _ = self.get_dummy_inputs(device)
+    for config in [
+        get_dense_test_config(position_embedding_type),
+        get_moe_test_config(position_embedding_type),
+    ]:
+        model = from_config(config, dtype=dtype).to(device)
+        model.eval()
 
-            model.generate(input_ids=input_ids, attention_mask=attention_mask)
+        input_ids, attention_mask, _ = get_dummy_inputs(device)
+
+        model.generate(input_ids=input_ids, attention_mask=attention_mask)
