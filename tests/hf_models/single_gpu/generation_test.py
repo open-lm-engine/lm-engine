51d2a6029f4d70ca53a7b01b14fac34c32bb4ead
Mayank Mishra <mayank31398@gmail.com>
2026-03-23 20:18:51 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/single_gpu/generation_test.py
+++ tests/hf_models/single_gpu/generation_test.py
@@ -5,8 +5,13 @@
 import pytest
 import torch
 
-from ...test_common import skip_test_if_device_unavailable
-from ...utils import from_config, get_dense_test_config, get_dummy_inputs, get_moe_test_config
+from ...utils import (
+    from_config,
+    get_dense_test_config,
+    get_dummy_inputs,
+    get_moe_test_config,
+    skip_test_if_device_unavailable,
+)
 
 
 @pytest.mark.parametrize("device", [torch.device("cpu"), torch.device("cuda")])
