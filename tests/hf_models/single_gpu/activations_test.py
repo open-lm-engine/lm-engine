fd829b6d1f44e9fde962c236e1dc9515783e8b7a
Mayank Mishra <mayank31398@gmail.com>
2026-03-23 19:36:40 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/single_gpu/activations_test.py
+++ tests/hf_models/single_gpu/activations_test.py
@@ -2,28 +2,29 @@
 # Copyright (c) 2025, Mayank Mishra
 # **************************************************
 
+import pytest
 import torch
-from parameterized import parameterized
 
 from lm_engine.hf_models.modeling_utils import get_activation_function
 from lm_engine.hf_models.modeling_utils.activations import get_base_activation
 from lm_engine.hf_models.modeling_utils.activations.glu import GLUActivation
 
-from ..test_common import TestCommons
+from ...test_common import skip_test_if_device_unavailable
+from ..test_common import assert_equal_tensors
 
 
-class ActivationsTest(TestCommons):
-    @parameterized.expand(TestCommons.make_args_matrix(TestCommons.get_all_devices(), [False, True]))
-    def test_sigmoid_glu(self, device: torch.device, is_interleaved: bool) -> None:
-        self.skip_test_if_device_unavailable(device)
+@pytest.mark.parametrize("device", [torch.device("cpu"), torch.device("cuda")])
+@pytest.mark.parametrize("is_interleaved", [False, True])
+def test_sigmoid_glu(device: torch.device, is_interleaved: bool) -> None:
+    skip_test_if_device_unavailable(device)
 
-        base_sigmoid = get_base_activation("sigmoid")
-        sigmoid_glu = GLUActivation(base_sigmoid)
+    base_sigmoid = get_base_activation("sigmoid")
+    sigmoid_glu = GLUActivation(base_sigmoid)
 
-        pytorch_glu = get_activation_function("glu")
+    pytorch_glu = get_activation_function("glu")
 
-        x = torch.randn(10, 10, device=device)
-        sigmoid_glu_output = sigmoid_glu(x, is_interleaved=is_interleaved)
-        pytorch_glu_output = pytorch_glu(x, is_interleaved=is_interleaved)
+    x = torch.randn(10, 10, device=device)
+    sigmoid_glu_output = sigmoid_glu(x, is_interleaved=is_interleaved)
+    pytorch_glu_output = pytorch_glu(x, is_interleaved=is_interleaved)
 
-        self.assert_equal_tensors(sigmoid_glu_output, pytorch_glu_output, True)
+    assert_equal_tensors(sigmoid_glu_output, pytorch_glu_output, True)
