dff7f69540205e5f9ad906fb883ed5bf3406adc1
Mayank Mishra <mayank31398@gmail.com>
2026-03-23 18:20:49 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/test_common.py
+++ tests/test_common.py
@@ -7,10 +7,20 @@ from itertools import product
 from typing import Any, Callable
 from unittest import TestCase, skipUnless
 
+import pytest
 import torch
 
 
 _RUN_SLOW = True if os.getenv("RUN_SLOW", "False").lower() in ["1", "true"] else False
+
+
+def skip_test_if_device_unavailable(device: torch.device) -> None:
+    # convert to str
+    if isinstance(device, torch.device):
+        device = device.type
+
+    if device == "cuda" and not torch.cuda.is_available():
+        pytest.skip("skipping test because CUDA is unavailable")
 
 
 class BaseTestCommons(TestCase):
@@ -24,14 +34,6 @@ class BaseTestCommons(TestCase):
 
     def make_args_matrix(*args_lists) -> list[Any]:
         return [p for p in product(*args_lists)]
-
-    def skip_test_if_device_unavailable(self, device: torch.device) -> None:
-        # convert to str
-        if isinstance(device, torch.device):
-            device = device.type
-
-        if device == "cuda" and not torch.cuda.is_available():
-            self.skipTest("skipping test because CUDA is unavailable")
 
     @staticmethod
     def slow_test(func: Callable) -> Callable:
