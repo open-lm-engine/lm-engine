04ebb028a166346928a8d299475ea3d46f1aa974
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2026-05-12 14:32:46 -0700

    [FIX] fix imports (#27)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/data/__init__.py
+++ lm_engine/data/__init__.py
@@ -2,5 +2,7 @@
 # Copyright (c) 2025, Mayank Mishra
 # **************************************************
 
+from .dataloader import ResumableDataLoader
 from .finetuning import get_finetuning_dataloader
 from .pretraining import get_pretraining_dataloaders
+from .utils import custom_iterator, get_next_batch
