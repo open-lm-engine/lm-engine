76ea1178542e3a8f89d4b7538742bdfdeab9f6e1
Mayank Mishra <mayank31398@gmail.com>
2026-05-17 02:34:26 -0700

    fix
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/model_conversion/__init__.py
+++ lm_engine/hf_models/model_conversion/__init__.py
@@ -1,5 +1,5 @@
 # **************************************************
-# Copyright (c) 2025, Mayank Mishra
+# Copyright (c) 2026, Mayank Mishra, Shawn Tan
 # **************************************************
 
 from transformers import AutoTokenizer, GenerationConfig
