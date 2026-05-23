ad653ac75554c5db8ccbf690f0ff4d6bdddaf0fb
Mayank Mishra <mayank31398@gmail.com>
2026-05-23 15:37:30 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/parallel/context_parallel/__init__.py
+++ lm_engine/parallel/context_parallel/__init__.py
@@ -2,6 +2,12 @@
 # Copyright (c) 2026, Mayank Mishra
 # **************************************************
 
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from typing import Any
 
 import torch
