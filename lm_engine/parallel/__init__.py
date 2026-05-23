05681168054885cd2b6fdffd055a4b20429cf9df
Mayank Mishra <mayank31398@gmail.com>
2026-05-23 15:35:24 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/parallel/__init__.py
+++ lm_engine/parallel/__init__.py
@@ -2,8 +2,6 @@
 # Copyright (c) 2026, Mayank Mishra
 # **************************************************
 
-from __future__ import annotations
-
 from .manager import ProcessGroupManager, run_rank_n
 from .pipeline_parallel import get_pipeline_stage_ids_on_current_rank
 from .simple_fsdp import MixedPrecisionPolicy, data_parallel, get_simple_fsdp_compile_backend
