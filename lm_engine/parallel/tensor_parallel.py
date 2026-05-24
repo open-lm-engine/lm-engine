b8841663a6e23b5d5b696fc10632659213e5df45
Mayank Mishra <mayank31398@gmail.com>
2026-05-23 19:30:11 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/parallel/tensor_parallel.py
+++ lm_engine/parallel/tensor_parallel.py
@@ -9,20 +9,20 @@ from .manager import ProcessGroupManager
 
 
 def broadcast_tensor_parallel_input(tokens: dict, shape: tuple[int]) -> torch.Tensor:
-    if ProcessGroupManager.is_tensor_parallel_enabled():
-        device = Accelerator.get_current_device()
+    device = Accelerator.get_current_device()
 
-        if ProcessGroupManager.is_tensor_parallel_first_rank():
-            tokens = tokens.to(device)
-        else:
-            tokens = torch.empty(shape, dtype=torch.long, device=device)
+    if not ProcessGroupManager.is_tensor_parallel_enabled():
+        return tokens.to(device)
 
-        torch.distributed.broadcast(
-            tokens,
-            src=ProcessGroupManager.get_tensor_parallel_first_rank(),
-            group=ProcessGroupManager.get_tensor_parallel_group(),
-        )
+    if ProcessGroupManager.is_tensor_parallel_first_rank():
+        tokens = tokens.to(device)
     else:
-        tokens = tokens.to(Accelerator.get_current_device())
+        tokens = torch.empty(shape, dtype=torch.long, device=device)
+
+    torch.distributed.broadcast(
+        tokens,
+        src=ProcessGroupManager.get_tensor_parallel_first_rank(),
+        group=ProcessGroupManager.get_tensor_parallel_group(),
+    )
 
     return tokens
