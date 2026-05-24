bfb9bc7cd64c5924e12103c31c86c3f0818d96aa
Mayank Mishra <mayank31398@gmail.com>
2026-05-24 12:37:30 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/utils/ring_attention.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/utils/ring_attention.py
@@ -23,81 +23,6 @@ def _ring_attention_forward(
     softcap: float,
     group: ProcessGroup,
 ) -> tuple[torch.Tensor, ...]:
-    """
-    A generalized ring attention implementation that can support multiple attention ops.
-
-    Note [Context parallelism load balance algorithm for causal masking]
-    =====================
-    This explanation uses an example to illustrate the CP algorithm with causal
-    masking.
-
-    Consider a scenario where the sequence length of q, k, and v is 4 (e.g.,
-    q = (q0, q1, q2, q3)), and there are two ranks. For simplicity, we will discuss
-    only q and k, as v follows the same pattern as k.
-
-    The diagram below represents a complete QK^T operation without parallelism.
-    The `****` entries indicate that the result is not required due to causal
-    masking (e.g., q0k1 is marked as `****`).
-
-    +----+------------------------+
-    |    |  k0    k1   k2     k3  |
-    +----+------------------------+
-    | q0 | q0k0, ****, ****, **** |
-    | q1 | q1k0, q1k1, ****, **** |
-    | q2 | q2k0, q2k1, q2k2, **** |
-    | q3 | q3k0, q3k1, q3k2, q3k3 |
-    +----+------------------------+
-
-    ### No Load Balance:
-
-    In this scenario, each rank owns a local chunk of q, k, and v, with each chunk
-    containing two elements. Rank0 is responsible for managing (q0, q1) and (k0, k1),
-    while rank1 manages (q2, q3) and (k2, k3).
-
-    First Iteration: Both rank0 and rank1 perform SDPA with their local qkv pairs.
-    Causal masking is enabled as some results are not required (e.g., q0k1).
-
-    Second Iteration: Local queries remain the same, but local kv pairs are exchanged.
-    Rank0 now has (q0, q1) and (k2, k3); rank1 has (q2, q3) and (k0, k1). Rank0 performs
-    no computation, while rank1 computes locally without causal masking since all results
-    (q2k0, q2k1, q3k0, q3k1) are needed.
-
-    ### Round-robin Load Balance:
-
-    In this setup, each rank owns two local chunks of q, k, and v, with each chunk
-    containing one element. Rank0 manages (q0, q3) and (k0, k3); Rank1 manages (q1, q2)
-    and (k1, k2). Although the local chunks are not consecutive, they are concatenated to
-    enable SDPA to be performed in a single call for each step. Consequently, the chunk()
-    function may be required to prepare the correct q, k, and v configurations.
-
-    First Iteration: Both ranks perform SDPA with their local qkv pairs, similar to the
-    no-load-balance case. This iteration corresponds to the `if` of the
-    (`if, `elif`, `else`) in the implementation.
-
-    Second Iteration: Rank0 now has (q0, q3) and (k1, k2); rank1 has (q1, q2) and
-    (k0, k3). For rank0, no computation is needed for q0. However, computations for
-    q3k1 and q3k2 are required, so only q3 is used for SDPA. This corresponds to the
-    `else` of the (`if`, `elif`, `else`) in the implementation.
-    For rank1, k3 is not needed for q1 and q2, so only k0 is used for SDPA. This
-    corresponds to the `elif` of (`if`, `elif`, `else`) in the implementation.
-
-    Parameters
-    ----------
-    op:
-        The attention op to use
-    *args:
-        additional args are passed to the op
-    **kwargs:
-        additional kwargs are passed to the op
-
-    Returns
-    -------
-    out:
-        The merged attention output
-    softmax_lse:
-        The logsumexp of the merged attention output
-    """
-
     _flash_attention_function, _flash_attention_varlen_function = _get_flash_attention_function(dropout=dropout)
 
     if causal and (q.size(1) != k.size(1)):
@@ -146,16 +71,16 @@ def _ring_attention_forward(
         if i == 0 or (not _cp_options.enable_load_balance or not is_causal):
             # When local balance is enabled, we still need to do SDPA with
             # the both local chunks of q, k, v for the first iteration.
-            q, k, v, partial = (query, key, value, False)
+            q, k, v, partial = (q, k, v, False)
         elif i <= rank:
             # Round-robin load balancing case, and i <= rank.
             # We need to do SDPA with only the first local chunk of k, v.
             # Note that q, k, v each contains two local chunks.
             ROUND_ROBIN_CYCLE = 2
             q, k, v, partial = (
-                query,
-                key.chunk(ROUND_ROBIN_CYCLE, dim=2)[0],
-                value.chunk(ROUND_ROBIN_CYCLE, dim=2)[0],
+                q,
+                k.chunk(ROUND_ROBIN_CYCLE, dim=2)[0],
+                v.chunk(ROUND_ROBIN_CYCLE, dim=2)[0],
                 False,
             )
         else:
@@ -163,17 +88,21 @@ def _ring_attention_forward(
             # We need to do SDPA with only the second half of q, and update
             # only the second part of logsumexp. So partial is True.
             # Note that q, k, v each contains two chunks.
-            q, k, v, partial = query.chunk(2, dim=2)[1], key, value, True
+            q, k, v, partial = q.chunk(2, dim=2)[1], k, v, True
 
         # See https://github.com/pytorch/pytorch/blob/release/2.4/aten/src/ATen/native/native_functions.yaml#L14695
         # for the SDPA kernel definitions.
-        out, logsumexp, *rest = op(
+        out, logsumexp, *rest = _flash_attention_function(
             q,
             k,
             v,
             is_causal=is_causal_behavior.value,
-            **kwargs,
+            softmax_scale=softmax_scale,
+            causal=causal,
+            window_size=window_size,
+            softcap=softcap,
         )
+
         sdpa_merger.step(out, logsumexp, partial)
 
     # pyrefly: ignore [unbound-name]
