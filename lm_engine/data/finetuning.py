874fa5875f68ffa1450b148acb6a39e816091470
Mayank Mishra <mayank31398@gmail.com>
2026-05-27 11:43:04 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ lm_engine/data/finetuning.py
@@ -0,0 +1,172 @@
+# **************************************************
+# Copyright (c) 2026, Mayank Mishra, Jyo Pari, Zhonglin Han
+# **************************************************
+
+import logging
+from functools import partial
+
+from ..arguments import DatasetArgs, TrainingArgs
+from ..enums import DatasetSplit
+from ..logging_utils import log_rank_0
+from ..parallel import ProcessGroupManager, run_rank_n
+from ..tokenizers import TOKENIZER_TYPE
+from .base import BaseDataset, BlendedDatasets
+from .dataloader import ResumableDataLoader
+from .debug import DebugDataset
+from .huggingface import HuggingFaceDataset
+from .sampler import BlendedDistributedSampler
+from .utils import collate_fn
+
+
+_DATASETS_LIST = {"DebugDataset": DebugDataset, "HuggingFaceDataset": HuggingFaceDataset}
+
+
+def get_datasets_list(
+    dataset_args_list: list[DatasetArgs], split: DatasetSplit, use_output: bool, tokenizer: TOKENIZER_TYPE
+) -> tuple[list[BaseDataset], list[int]]:
+    """get the list of datasets from their configs
+
+    Args:
+        dataset_args_list (list[DatasetArgs]): list of DatasetArgs objects
+        split (DatasetSplit): train / val / test split
+        use_output (bool): whether the output is returned in the dataset's samples
+        tokenizer (TOKENIZER_TYPE): tokenizer
+
+    Raises:
+        ValueError: if invalid class_name for dataset is found
+
+    Returns:
+        tuple[List[BaseDataset], list[int]]: tuple of list of datasets and the respective dataset sampling ratios
+    """
+
+    datasets_list = []
+    data_sampling_ratios = []
+    for data_args in dataset_args_list:
+        if data_args.class_name not in _DATASETS_LIST:
+            raise ValueError(f"invalid class_name ({data_args.class_name}) for dataset")
+
+        dataset = _DATASETS_LIST[data_args.class_name](
+            class_args=data_args.class_args,
+            split=split,
+            use_output=use_output,
+            tokenizer=tokenizer,
+            data_name=data_args.data_name,
+            input_format=data_args.input_format,
+            output_format=data_args.output_format,
+            max_input_tokens=data_args.max_input_tokens,
+            max_output_tokens=data_args.max_output_tokens,
+        )
+
+        if len(dataset) > 0:
+            datasets_list.append(dataset)
+            data_sampling_ratios.append(data_args.data_sampling_ratio)
+
+            log_rank_0(
+                logging.INFO, f"examples in {dataset.__class__.__name__} ({data_args.data_name}) = {len(dataset)}"
+            )
+
+    assert all([i is not None for i in data_sampling_ratios]) or all(
+        [i is None for i in data_sampling_ratios]
+    ), "either all data_sampling_ratios should be specified or all should be None"
+    if all([i is None for i in data_sampling_ratios]):
+        data_sampling_ratios = [len(i) for i in datasets_list]
+
+    return datasets_list, data_sampling_ratios
+
+
+def get_finetuning_dataloader(
+    args: TrainingArgs, split: DatasetSplit, use_output: bool, tokenizer: TOKENIZER_TYPE
+) -> ResumableDataLoader:
+    """prepares datasets and sampler
+
+    Args:
+        args (TrainingArgs): arguments based on training / inference mode
+        split (DatasetSplit): train / val / test split
+        use_output (bool): whether dataloader can return output in the sample
+        tokenizer (TOKENIZER_TYPE): tokenizer
+
+    Returns:
+        ResumableDataLoader: dataloader for a blended dataset
+    """
+
+    if ProcessGroupManager.get_tensor_parallel_rank() != 0:
+        return
+
+    micro_batch_size = args.training_parameters.micro_batch_size
+
+    datasets_list, data_sampling_ratios = get_datasets_list(
+        dataset_args_list=args.datasets, split=split, use_output=use_output, tokenizer=tokenizer
+    )
+
+    if len(datasets_list) == 0:
+        return None
+
+    blended_dataset = BlendedDatasets(datasets=datasets_list, split=split)
+
+    # routing to data parallel worker is done by sampler
+    sampler = BlendedDistributedSampler(
+        dataset=blended_dataset,
+        data_sampling_ratios=[1] if len(datasets_list) == 1 else data_sampling_ratios,
+        num_replicas=ProcessGroupManager.get_data_parallel_world_size(),
+        rank=ProcessGroupManager.get_data_parallel_rank(),
+        ignore_sampling_proportion_for_validation=args.training_parameters.ignore_sampling_proportion_for_validation,
+        shuffle=split == DatasetSplit.train,
+        seed=args.random_args.seed,
+        drop_last=False,
+    )
+
+    # dataloader is unaware of data parallel routing
+    dataloader = ResumableDataLoader(
+        blended_dataset,
+        batch_size=micro_batch_size,
+        sampler=sampler,
+        collate_fn=partial(
+            collate_fn,
+            use_output=use_output,
+            loss_mask=args.training_parameters.loss_mask,
+            eos_token_id=tokenizer.eos_token_id,
+            use_padding_free_transformer=args.model_args.use_padding_free_transformer,
+            pad_to_multiple_of=ProcessGroupManager.get_tensor_parallel_world_size(),
+        ),
+    )
+
+    _log_dataset(
+        blended_dataset=blended_dataset,
+        sampler=sampler,
+        split=split,
+        num_training_steps=args.training_parameters.num_training_steps,
+        gradient_accumulation_steps=args.training_parameters.gradient_accumulation_steps,
+        micro_batch_size=args.training_parameters.micro_batch_size,
+    )
+
+    return dataloader
+
+
+@run_rank_n
+def _log_dataset(
+    blended_dataset: BlendedDatasets,
+    sampler: BlendedDistributedSampler,
+    split: DatasetSplit,
+    num_training_steps: int,
+    gradient_accumulation_steps: int,
+    micro_batch_size: int,
+) -> None:
+    log_rank_0(logging.INFO, f"{'-' * 25} {split.value} {'-' * 25}")
+    log_rank_0(logging.INFO, blended_dataset)
+
+    dp_world_size = ProcessGroupManager.get_data_parallel_world_size()
+
+    if split == DatasetSplit.train:
+        total_samples_seen = num_training_steps * gradient_accumulation_steps * micro_batch_size * dp_world_size
+    else:
+        num_steps = len(blended_dataset) // (micro_batch_size * dp_world_size)
+        if len(blended_dataset) % (micro_batch_size * dp_world_size) != 0:
+            num_steps += 1
+
+        total_samples_seen = num_steps * micro_batch_size * dp_world_size
+
+    log_rank_0(logging.INFO, "*" * 57)
+    log_rank_0(logging.INFO, f"total samples seen = {total_samples_seen}")
+    log_rank_0(logging.INFO, f"total epochs for the dataset mixture = {total_samples_seen / len(blended_dataset)}")
+    log_rank_0(logging.INFO, sampler)
+    log_rank_0(logging.INFO, "-" * 57)
---
+++ lm_engine/data/pretraining.py
@@ -0,0 +1,209 @@
+# **************************************************
+# Copyright (c) 2026, Mayank Mishra, Jyo Pari, Zhonglin Han
+# **************************************************
+
+import logging
+
+from ..accelerator import Accelerator
+from ..arguments import TrainingArgs
+from ..defaults import INPUT_FORMAT, OUTPUT_FORMAT
+from ..logging_utils import log_rank_0
+from ..parallel import ProcessGroupManager
+from ..tokenizers import TOKENIZER_TYPE
+from .dataloader import ResumableDataLoader
+from .megatron import GPTDataset, GPTDatasetConfig, MegatronBatchSampler, build, compile_helpers
+
+
+def _get_train_val_test_samples(
+    num_training_steps: int,
+    micro_batch_size: int,
+    gradient_accumulation_steps: int,
+    eval_interval: int,
+    eval_steps: int,
+) -> tuple[int, int, int]:
+    dp_world_size = ProcessGroupManager.get_data_parallel_world_size()
+
+    train_samples = num_training_steps * micro_batch_size * gradient_accumulation_steps * dp_world_size
+    val_samples = (
+        (num_training_steps // eval_interval + 1)
+        * eval_steps
+        * micro_batch_size
+        * gradient_accumulation_steps
+        * dp_world_size
+    )
+    test_samples = eval_steps * micro_batch_size * gradient_accumulation_steps * dp_world_size
+
+    return train_samples, val_samples, test_samples
+
+
+def _get_dataloader(
+    dataset: GPTDataset | StitchedSequenceDataset | None,
+    consumed_samples: int,
+    micro_batch_size: int,
+    gradient_accumulation_steps: int,
+    num_pipeline_stages: int,
+    num_workers: int = 2,
+):
+    if dataset is None:
+        return None
+
+    accelerator = Accelerator.get_accelerator()
+
+    if accelerator in [Accelerator.mps, Accelerator.trainium]:
+        num_workers = 0
+
+    dataloader = ResumableDataLoader(
+        dataset,
+        batch_sampler=MegatronBatchSampler(
+            total_samples=len(dataset),
+            consumed_samples=consumed_samples,
+            micro_batch_size=(
+                micro_batch_size if num_pipeline_stages == 1 else micro_batch_size * gradient_accumulation_steps
+            ),
+            num_replicas=ProcessGroupManager.get_data_parallel_world_size(),
+            rank=ProcessGroupManager.get_data_parallel_rank(),
+        ),
+        multiprocessing_context="fork" if accelerator == Accelerator.tpu else None,
+        num_workers=num_workers,
+        pin_memory=accelerator not in [Accelerator.mps, Accelerator.trainium],
+    )
+
+    return iter(dataloader)
+
+
+def get_pretraining_dataloaders(
+    args: TrainingArgs, tokenizer: TOKENIZER_TYPE, consumed_samples: int
+) -> tuple[ResumableDataLoader, list[ResumableDataLoader], list[ResumableDataLoader]]:
+    assert len(args.datasets) == 1
+    class_args = args.datasets[0].class_args
+
+    assert args.datasets[0].max_input_tokens is None
+    assert args.datasets[0].max_output_tokens is None
+    assert args.datasets[0].max_output_tokens is None
+    assert args.datasets[0].input_format == INPUT_FORMAT
+    assert args.datasets[0].output_format == OUTPUT_FORMAT
+
+    micro_batch_size = args.training_parameters.micro_batch_size
+    gradient_accumulation_steps = args.training_parameters.gradient_accumulation_steps
+    num_pipeline_stages = args.distributed_args.num_pipeline_stages
+    num_workers = class_args.get("num_workers", 2)
+
+    is_megatron = args.datasets[0].class_name == "MegatronDataset"
+
+    if is_megatron:
+        compile_helpers()
+
+        log_rank_0(logging.INFO, "> building train, validation, and test datasets for GPT ...")
+
+        # Option 1: data loading using --data-path with single file
+        # Option 2: data loading using --data-path with multiple weighted files
+        # Option 3: data loading using --(train|val|test)-data-path with multiple weighted files
+        train_ds, val_ds, test_ds = build(
+            sizes=_get_train_val_test_samples(
+                args.training_parameters.num_training_steps,
+                micro_batch_size,
+                gradient_accumulation_steps,
+                args.training_parameters.eval_interval,
+                class_args.get("eval_steps"),
+            ),
+            config=GPTDatasetConfig(
+                sequence_length=class_args.get("sequence_length"),
+                blend=class_args.get("data_path"),
+                blend_per_split=[
+                    class_args.get("train_data_path"),
+                    class_args.get("val_data_path"),
+                    class_args.get("test_data_path"),
+                ],
+                split=class_args.get("split"),
+                path_to_cache=class_args.get("data_cache_path"),
+                fim_rate=class_args.get("fim_rate", 0),
+                fim_spm_rate=class_args.get("fim_spm_rate", 0.5),
+            ),
+            tokenizer=tokenizer,
+            node_uses_local_storage=class_args.get("node_uses_local_storage", False),
+            random_seed=class_args.get("seed", args.random_args.seed),
+        )
+
+        if not isinstance(val_ds, list):
+            val_ds = [val_ds]
+        if not isinstance(test_ds, list):
+            test_ds = [test_ds]
+    else:
+        raise ValueError
+
+    log_rank_0(logging.INFO, "> finished creating GPT datasets ...")
+
+    if is_megatron:
+        train_ds = _get_dataloader(
+            train_ds,
+            consumed_samples=consumed_samples,
+            micro_batch_size=micro_batch_size,
+            gradient_accumulation_steps=gradient_accumulation_steps,
+            num_pipeline_stages=num_pipeline_stages,
+            num_workers=num_workers,
+        )
+
+        val_ds = [
+            _get_dataloader(
+                i,
+                consumed_samples=0,
+                micro_batch_size=micro_batch_size,
+                gradient_accumulation_steps=gradient_accumulation_steps,
+                num_pipeline_stages=num_pipeline_stages,
+                num_workers=num_workers,
+            )
+            for i in val_ds
+        ]
+
+        test_ds = [
+            _get_dataloader(
+                i,
+                consumed_samples=0,
+                micro_batch_size=micro_batch_size,
+                gradient_accumulation_steps=gradient_accumulation_steps,
+                num_pipeline_stages=num_pipeline_stages,
+                num_workers=num_workers,
+            )
+            for i in test_ds
+        ]
+    else:
+        train_ds = _get_dataloader(
+            train_ds,
+            consumed_samples=consumed_samples,
+            micro_batch_size=micro_batch_size,
+            gradient_accumulation_steps=gradient_accumulation_steps,
+            num_pipeline_stages=num_pipeline_stages,
+            num_workers=num_workers,
+        )
+
+        val_ds = (
+            [
+                _get_dataloader(
+                    val_ds,
+                    consumed_samples=0,
+                    micro_batch_size=micro_batch_size,
+                    gradient_accumulation_steps=gradient_accumulation_steps,
+                    num_pipeline_stages=num_pipeline_stages,
+                    num_workers=num_workers,
+                )
+            ]
+            if val_ds is not None
+            else [None]
+        )
+
+        test_ds = (
+            [
+                _get_dataloader(
+                    test_ds,
+                    consumed_samples=0,
+                    micro_batch_size=micro_batch_size,
+                    gradient_accumulation_steps=gradient_accumulation_steps,
+                    num_pipeline_stages=num_pipeline_stages,
+                    num_workers=num_workers,
+                )
+            ]
+            if test_ds is not None
+            else [None]
+        )
+
+    return train_ds, val_ds, test_ds
