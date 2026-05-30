e681020e95795e6c41965beb83b019509c08908d
Mayank Mishra <mayank31398@gmail.com>
2026-05-30 10:35:50 -0700

    slides
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/attention/all_to_all.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/attention/all_to_all.py
@@ -5,9 +5,37 @@
 from __future__ import annotations
 
 import torch
-from torch.distributed._functional_collectives import AsyncCollectiveTensor, permute_tensor
+from torch.distributed._functional_collectives import RANK_TYPES, AsyncCollectiveTensor
+from torch.distributed._functional_collectives import permute_tensor
+from torch.distributed._functional_collectives import permute_tensor as _permute_tensor_no_grad
 
 from .....parallel import ProcessGroupManager
+
+
+class _PermuteTensor(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, src_dst: list[int], group: RANK_TYPES) -> torch.Tensor:
+        ctx.src_dst = src_dst
+        x = permute_tensor(x, src_dst=src_dst, group=ProcessGroupManager.get_context_parallel_group())
+        return x
+
+    @staticmethod
+    def backward(ctx, dx: torch.Tensor):
+        src_dst = ctx.src_dst
+        inv_src_dst = [0] * len(src_dst)
+        for src, dst in enumerate(src_dst):
+            inv_src_dst[dst] = src
+        dx = permute_tensor(dx, src_dst=inv_src_dst, group=ProcessGroupManager.get_context_parallel_group())
+        return dx, None, None
+
+
+def permute_tensor(x: torch.Tensor, src_dst: list[int], group: RANK_TYPES, with_grad: bool) -> torch.Tensor:
+    if with_grad:
+        x = _PermuteTensor.apply(x, src_dst, group)
+    else:
+        x = _permute_tensor_no_grad(x, src_dst=src_dst, group=group)
+
+    return x
 
 
 class AllToAllRotater:
@@ -17,10 +45,12 @@ class AllToAllRotater:
         self._seq_dim = seq_dim
         self._buffer: torch.Tensor | None = None
 
-    def exchange_buffers(self, curr_buffer: torch.Tensor) -> None:
+    def exchange_buffers(self, curr_buffer: torch.Tensor, with_grad: bool) -> None:
         curr_buffer = curr_buffer.contiguous()
         dsts = list(range(1, ProcessGroupManager.get_context_parallel_world_size())) + [0]
-        self._buffer = permute_tensor(curr_buffer, dsts, ProcessGroupManager.get_context_parallel_group())
+        self._buffer = permute_tensor(
+            curr_buffer, src_dst=dsts, group=ProcessGroupManager.get_context_parallel_group(), with_grad=with_grad
+        )
 
     def next_buffer(self) -> torch.Tensor:
         assert self._buffer is not None
