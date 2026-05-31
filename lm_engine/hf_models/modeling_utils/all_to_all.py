2831654d2772d6fe8fe190d9be94aa7967724af9
Mayank Mishra <mayank31398@gmail.com>
2026-05-30 23:42:46 -0700

    slides
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/all_to_all.py
+++ lm_engine/hf_models/modeling_utils/all_to_all.py
@@ -5,7 +5,13 @@
 from __future__ import annotations
 
 import torch
-from torch.distributed._functional_collectives import RANK_TYPES, AsyncCollectiveTensor
+import torch.distributed.distributed_c10d as c10d
+from torch.distributed._functional_collectives import (
+    RANK_TYPES,
+    AsyncCollectiveTensor,
+    _expand_group,
+    all_to_all_single_autograd,
+)
 from torch.distributed._functional_collectives import permute_tensor as _permute_tensor_no_grad
 
 from ...parallel import ProcessGroupManager
@@ -35,7 +41,20 @@ class _PermuteTensor(torch.autograd.Function):
 
 def permute_tensor(x: torch.Tensor, src_dst: list[int], group: RANK_TYPES, with_grad: bool) -> torch.Tensor:
     if with_grad:
-        x = _PermuteTensor.apply(x, src_dst, group)
+        t, rankset, group_size = _expand_group(group)
+        local_pg = c10d._find_or_create_pg_by_ranks_and_tag(t, rankset, group_size)
+
+        output_split_sizes = [0] * group_size
+        input_split_sizes = [0] * group_size
+        for src, dst in enumerate(src_dst):
+            if src == torch.distributed.get_rank(local_pg):
+                input_split_sizes[dst] = x.numel()
+            if dst == torch.distributed.get_rank(local_pg):
+                output_split_sizes[src] = x.numel()
+
+        x = all_to_all_single_autograd(
+            x, output_split_sizes=output_split_sizes, input_split_sizes=input_split_sizes, group=group
+        )
     else:
         x = _permute_tensor_no_grad(x, src_dst=src_dst, group=group)
 
