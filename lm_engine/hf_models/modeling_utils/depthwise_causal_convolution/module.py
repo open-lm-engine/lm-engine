37d662270e36ec0b75b374fdec38663622146775
Mayank Mishra <mayank31398@gmail.com>
2026-05-31 12:07:21 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/depthwise_causal_convolution/module.py
+++ lm_engine/hf_models/modeling_utils/depthwise_causal_convolution/module.py
@@ -14,6 +14,7 @@ from ....parallel import ProcessGroupManager
 from ....utils import is_causal_conv1d_available
 from ...parameter import mark_parameter_as_initialized, mark_parameter_as_no_weight_decay
 from ..activations import get_activation_function
+from ..all_gather import AllGatherRotater
 from ..all_to_all import AllToAllRotater
 
 
@@ -90,7 +91,7 @@ class DepthwiseCausalConvolution(nn.Conv1d):
 
         if input_state is None:
             if is_cp_enabled and self.kernel_size > 1:
-                rotater = AllToAllRotater()
+                rotater = AllGatherRotater()
                 tail = x[:, 1 - self.kernel_size :]
                 rotater.exchange_buffers(tail.flatten(), with_grad=True)
 
