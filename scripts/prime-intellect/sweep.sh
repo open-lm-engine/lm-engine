c79159fbb60171b85dcb95817a8560059da3d625
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2026-06-09 17:33:18 -0700

    [CLEANUP] drop old sweep (#462)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/prime-intellect/sweep.sh
+++ scripts/prime-intellect/sweep.sh
@@ -1,8 +1,8 @@
 export TMPDIR=tmp
 
 python tools/wandb_sweep.py \
-    --config configs/base.yml \
-    --sweep configs/sweep.yml \
+    --config ${1} \
+    --sweep configs/sweep/sweep.yml \
     --slurm_logs_dir sweep \
     --count 100 \
     --max_concurrent 8 \
