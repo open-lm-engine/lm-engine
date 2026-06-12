cff860e10f9e950bb5bb92a4141739246c0f6e88
Mayank Mishra <mayank31398@gmail.com>
2026-06-11 19:26:33 -0700

    drop
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/models/__init__.py
+++ lm_engine/hf_models/models/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2026, Mayank Mishra
 # **************************************************
 
-from .gpt_base import GPTBaseConfig, GPTBaseForCausalLM, GPTBaseModel, unshard_gpt_base_tensor_parallel_state_dicts
+from .gpt_base import GPTBaseConfig, GPTBaseForCausalLM, GPTBaseModel
 from .gpt_crosslayer import GPTCrossLayerConfig, GPTCrossLayerForCausalLM, GPTCrossLayerModel
 from .ladder_residual import LadderResidualConfig, LadderResidualForCausalLM, LadderResidualModel
 from .palm import PaLMConfig, PaLMForCausalLM, PaLMModel
