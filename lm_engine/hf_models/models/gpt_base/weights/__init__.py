cd969f6b8d3eec926a38c29c7ddb1371fef693de
Mayank Mishra <mayank31398@gmail.com>
2026-06-11 19:22:56 -0700

    drop
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/models/gpt_base/weights/__init__.py
+++
@@ -1,5 +0,0 @@
-# **************************************************
-# Copyright (c) 2026, Mayank Mishra
-# **************************************************
-
-from .shard import get_gpt_base_model_parallel_state_dict
