a545c1d8b4b8031224effa4d36df6f8fe703807e
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2026-06-17 20:20:24 -0700

    [REFACTOR] refactor convolution (#471)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/depthwise_causal_convolution.py
+++ lm_engine/hf_models/modeling_utils/depthwise_causal_convolution.py
@@ -8,13 +8,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from ....enums import Kernel
-from ....kernels import is_kernel_allowed
-from ....parallel import ProcessGroupManager
-from ....utils import is_causal_conv1d_available
-from ...parameter import mark_parameter_as_initialized, mark_parameter_as_no_weight_decay
-from ..activations import get_activation_function
-from ..rotaters import AllGatherRotater
+from ...enums import Kernel
+from ...kernels import is_kernel_allowed
+from ...parallel import ProcessGroupManager
+from ...utils import is_causal_conv1d_available
+from ..parameter import mark_parameter_as_initialized, mark_parameter_as_no_weight_decay
+from .activations import get_activation_function
+from .rotaters import AllGatherRotater
 
 
 if is_causal_conv1d_available():
