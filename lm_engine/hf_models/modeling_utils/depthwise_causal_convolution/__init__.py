7de548a5d5e17252e724e43060e38d1cd2e1f382
Mayank Mishra <mayank31398@gmail.com>
2026-06-17 20:19:43 -0700

    revert
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/hf_models/modeling_utils/depthwise_causal_convolution/__init__.py
+++
@@ -1,5 +0,0 @@
-# **************************************************
-# Copyright (c) 2026, Mayank Mishra
-# **************************************************
-
-from .module import DepthwiseCausalConvolution, _apply_mask_to_padding_states
