ed572abaa3e9cd6c95f11315f91862f7a4100e5b
Mayank Mishra <mayank31398@gmail.com>
2026-06-24 12:34:36 -0700

    revert
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/__init__.py
+++ lm_engine/__init__.py
@@ -1,3 +1,7 @@
+# **************************************************
+# Copyright (c) 2026, Mayank Mishra
+# **************************************************
+
 from .register_hf import register_model_classes
 
 
