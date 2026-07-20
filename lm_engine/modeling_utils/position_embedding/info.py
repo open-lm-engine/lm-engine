f6306bc405ed09b450c5894d60d877cce28826a1
Mayank Mishra <mayank31398@gmail.com>
2026-07-19 23:09:45 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/modeling_utils/position_embedding/info.py
+++ lm_engine/modeling_utils/position_embedding/info.py
@@ -6,8 +6,42 @@ from dataclasses import dataclass
 
 import torch
 
+from ...accelerator import Accelerator
+from ...parallel import ProcessGroupManager, prepare_context_parallel_input
+
 
 @dataclass
 class PositionInfo:
     position_ids: torch.Tensor | None
     rope_cos_sin: torch.Tensor | None
+
+    def get_position_ids(
+        self, attention_mask: torch.Tensor, past_length: int, query_length: int, key_length: int, device: torch.device
+    ) -> torch.Tensor:
+        if self.position_ids is None:
+            if attention_mask is not None and attention_mask.dim() == 2:
+                assert not ProcessGroupManager.is_context_parallel_enabled()
+                # create position_ids on the fly for batch generation
+                position_ids = (
+                    attention_mask.to(
+                        torch.int32 if Accelerator.get_accelerator() == Accelerator.trainium else torch.int64
+                    ).cumsum(-1)
+                    - 1
+                )
+
+                position_ids.masked_fill_(attention_mask == 0, 0)
+                if past_length > 0:
+                    position_ids = position_ids[:, past_length:key_length:]
+            else:
+                position_ids = torch.arange(
+                    past_length,
+                    key_length,
+                    dtype=torch.int32 if Accelerator.get_accelerator() == Accelerator.trainium else torch.int64,
+                    device=device,
+                )
+
+                position_ids = position_ids.unsqueeze(0).view(-1, query_length)
+
+            self.position_ids = prepare_context_parallel_input(inputs=(position_ids,))[0]
+
+        return self.position_ids
