9cb8e2b1dc858aadd1d46d8fa42622db323352d6
Mayank Mishra <mayank31398@gmail.com>
2026-07-31 13:59:34 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/modeling_utils/rotaters/__init__.py
+++ lm_engine/modeling_utils/rotaters/__init__.py
@@ -4,4 +4,4 @@
 
 from .all_gather import AllGatherRotater
 from .all_to_all import AllToAllRotater
-from .paired_send_recv_rotater import PairedSendRecvRotater
+from .send_recv_rotater import PairedSendRecvRotater
