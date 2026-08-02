4e047f96cc7fabbc9eb0543d679bd7559cdc85f2
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2026-08-02 03:21:30 -0700

    [M2RNN] add context parallel for M2RNN (#499)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/modeling_utils/sequence_mixer_blocks/mamba2/module.py
+++ lm_engine/modeling_utils/sequence_mixer_blocks/mamba2/module.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 from ....enums import Kernel
 from ....generation_cache import ConstantCache, GenerationCache, GenerationState
 from ....kernels import is_kernel_allowed
-from ....parallel import ProcessGroupManager
 from ....parameter import (
     mark_parameter_as_initialized,
     mark_parameter_as_mup_learning_rate,
