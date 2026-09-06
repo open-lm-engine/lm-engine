0854e2cd3b11db93bbf18e9469be4303c4b1f8ba
Mayank Mishra <mayank31398@gmail.com>
2026-09-06 03:33:59 -0700

    add gru
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/scattermoe_test.py
+++ tests/kernels/scattermoe_test.py
@@ -14,10 +14,16 @@ import torch.nn.functional as F
 from lm_engine.accelerator import Accelerator
 from lm_engine.enums import Kernel
 from lm_engine.kernels import enable_kernels, is_kernel_allowed
+from lm_engine.modeling_utils import AttentionMaskInfo
 from lm_engine.modeling_utils.activations import get_activation_function
 from lm_engine.modeling_utils.mlp_blocks.moe.experts import ColumnParallelExperts, RowParallelExperts
-from tests.layers.utils import assert_equal_tensors
-from tests.utils import skip_test_if_device_unavailable
+from tests.utils import (
+    assert_equal_tensors,
+    from_config,
+    get_dummy_inputs,
+    get_moe_test_config,
+    skip_test_if_device_unavailable,
+)
 
 
 _SEED = 42
@@ -215,3 +221,37 @@ def test_scattermoe_experts_forward_backward(
             atol_bfloat16=7.9e-3,
             rtol_bfloat16=0,
         )
+
+
+@pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16, torch.float16])
+def test_scattermoe(dtype: torch.dtype) -> None:
+    device = torch.device("cuda")
+    skip_test_if_device_unavailable(device)
+
+    Accelerator.set_seed(1234)
+
+    input_ids, attention_mask, _ = get_dummy_inputs(device)
+
+    config = get_moe_test_config("rope", num_layers=1, add_bias=False)
+
+    model = from_config(config, dtype=dtype).to(device)
+    model.eval()
+
+    naive_output = model(input_ids=input_ids, attention_mask_info=AttentionMaskInfo(attention_mask=attention_mask))
+
+    with enable_kernels([Kernel.scattermoe]):
+        scatter_output = model(
+            input_ids=input_ids, attention_mask_info=AttentionMaskInfo(attention_mask=attention_mask)
+        )
+
+    assert_equal_tensors(
+        naive_output.logits,
+        scatter_output.logits,
+        False,
+        rtol_float32=1e-3,
+        atol_float32=2e-4,
+        rtol_float16=0,
+        atol_float16=2.5e-4,
+        rtol_bfloat16=0,
+        atol_bfloat16=2e-3,
+    )
