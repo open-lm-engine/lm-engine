3d928055bfd41f2775ec640176de6847a4f6380d
Mayank Mishra <mayank31398@gmail.com>
2026-09-05 19:18:25 -0700

    add m2rnn
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/modeling_utils/sequence_mixer_blocks/m2rnn/op.py
+++ lm_engine/modeling_utils/sequence_mixer_blocks/m2rnn/op.py
@@ -4,63 +4,117 @@
 
 import torch
 
-from ....utils import divide_if_divisible
-from ...activations import clip_gradients, tanh
+from .....accelerator import KernelBackend
+from ....custom_op import CustomOp
+from ....utils import is_triton_available
+from ..utils import _get_num_heads
+from .torch_implementation import _m2rnn_torch
 
 
-def m2rnn_torch(
-    q: torch.Tensor,
-    k: torch.Tensor,
-    v: torch.Tensor,
-    xf: torch.Tensor,
-    W: torch.Tensor,
-    h0: torch.Tensor | None,
-    gradient_clipping: float | None,
+class _M2RNN(CustomOp): ...
+
+
+_M2RNN[KernelBackend.torch] = _m2rnn_torch
+
+
+if is_triton_available():
+    from .triton_implementation import _M2RNNTriton
+
+    _M2RNN[KernelBackend.cuda] = _M2RNNTriton
+    _M2RNN[KernelBackend.triton] = _M2RNNTriton
+
+
+def m2rnn(
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    weight: torch.Tensor,
+    forget_input: torch.Tensor,
+    input_state: torch.Tensor | None = None,
+    gradient_clipping: float | None = None,
+    cu_seqlens: torch.Tensor | None = None,
+    max_seqlen: torch.Tensor | int | None = None,
+    *,
+    kernel_backend: KernelBackend | None = None,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    B, S, Nq, K = q.size()
-    Nk = k.size(-2)
-    Nv = v.size(-2)
+    """
+    computes M2RNN recurrence
 
-    Nw = W.size(0)
-    Nxf = xf.size(-1)
+    :param query: query tensor of shape (B, S, Nq, K) where Nq is the number of query heads and K is the key head
+        dimension. Should have shape (T, Nq, K) and `cu_seqlens` should be passed.
+    :type query: torch.Tensor
+    :param key: key tensor of shape (B, S, Nk, K) where Nk is the number of key heads and K is the key head
+        dimension. Should have shape (T, Nk, K) and `cu_seqlens` should be passed.
+    :type key: torch.Tensor
+    :param value: value tensor of shape (B, S, Nv, V) where Nv is the number of value heads and V is the value head
+        dimension. Should have shape (T, Nv, V) and `cu_seqlens` should be passed.
+    :type value: torch.Tensor
+    :param weight: weight tensor of shape (Nw, V, V)
+    :type weight: torch.Tensor
+    :param forget_input: forget input tensor of shape (B, S, Nxf) where Nxf is the number of forget heads and H is the head
+        dimension. Should have shape (T, Nxf) and `cu_seqlens` should be passed.
+    :type forget_input: torch.Tensor
+    :param input_state: starting state of shape (B, N, K, V), where N = max{Nq, Nk, Nv, Nxf, Nw}. None means starting state is
+        0 tensor. Defaults to None.
+    :type input_state: torch.Tensor | None
+    :param gradient_clipping: gradient clipping for the state gradient in backward, None implies no clipping.
+        Defaults to None.
+    :type gradient_clipping: float | None
+    :param cu_seqlens: cumulative sequence length (must contain 0 as first element). Defaults to None.
+    :type cu_seqlens: torch.Tensor | None
+    :param max_seqlen: max sequence length in the batch. Defaults to None.
+    :type max_seqlen: int | None
+    :param kernel_backend: KernelBackend
+    :type kernel_backend: KernelBackend | None
+    :return: output tensor of shape (B, S, N, V) if `cu_seqlens` is None else (T, N, V) and output state of
+        shape (B, N, K, V).
+    :rtype: tuple[Tensor, Tensor]
+    """
 
-    N = max(Nq, Nk, Nv, Nw, Nxf)
-    V = v.size(-1)
-    y = torch.empty(B, S, N, K, V, device=q.device, dtype=q.dtype)
+    if cu_seqlens is None:
+        assert max_seqlen is None
+        B, S, _, K = query.size()
+    else:
+        assert max_seqlen is not None
+        assert cu_seqlens.dim() == 1
 
-    if h0 is None:
-        h0 = torch.zeros(B, N, K, V, device=k.device, dtype=k.dtype)
+        B = cu_seqlens.size(0) - 1
+        T, _, K = query.size()
 
-    Gq = divide_if_divisible(N, Nq)
-    Gk = divide_if_divisible(N, Nk)
-    Gv = divide_if_divisible(N, Nv)
+    V = value.size(-1)
 
-    Gw = divide_if_divisible(N, Nw)
-    Gxf = divide_if_divisible(N, Nxf)
+    Nq, Nk, Nv, Nw, Nxf, N = _get_num_heads(q=query, k=key, v=value, W=weight, xf=forget_input, run_check=True)
 
-    q = q.repeat_interleave(Gq, dim=-2)
-    k = k.repeat_interleave(Gk, dim=-2)
-    v = v.repeat_interleave(Gv, dim=-2)
-    W = W.repeat_interleave(Gw, dim=0)
-    xf = xf.repeat_interleave(Gxf, dim=-1)
+    if cu_seqlens is None:
+        assert query.size() == (B, S, Nq, K)
+        assert key.size() == (B, S, Nk, K)
+        assert value.size() == (B, S, Nv, V)
+        assert forget_input.size() == (B, S, Nxf)
+    else:
+        assert query.size() == (T, Nq, K)
+        assert key.size() == (T, Nk, K)
+        assert value.size() == (T, Nv, V)
+        assert forget_input.size() == (T, Nxf)
 
-    # (B, S, N, K, V) = (B, S, N, K, 1) * (B, S, N, 1, V)
-    x = k[..., None] * v[..., None, :]
-    W = W[None, ...]
+    assert weight.size() == (Nw, V, V)
 
-    for s in range(S):
-        f = xf[:, s, :, None, None]
-        # (B, N, K, V) = (B, N, K, V) @ (1, N, V, V) + (B, N, K, V)
-        h = h0 @ W + x[:, s]
-        h = tanh(h)
-        h = f * h0 + (1 - f) * h
+    if input_state is not None:
+        assert input_state.size() == (B, N, K, V)
 
-        h = clip_gradients(h, gradient_clipping)
+    if gradient_clipping is not None and gradient_clipping < 0:
+        gradient_clipping = -gradient_clipping
 
-        y[:, s] = h
-        h0 = h
+    output, input_state = _M2RNN.run(
+        q=query,
+        k=key,
+        v=value,
+        W=weight,
+        xf=forget_input,
+        h0=input_state,
+        gradient_clipping=gradient_clipping,
+        cu_seqlens=cu_seqlens,
+        max_seqlen=max_seqlen,
+        kernel_backend=kernel_backend,
+    )
 
-    y = q[..., None, :] @ y
-    y = y.squeeze(-2)
-
-    return y, h0
+    return output, input_state
