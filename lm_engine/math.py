45fa424437582219b8626bba9d904b6332e917d0
Mayank Mishra <mayank31398@gmail.com>
2026-09-05 20:48:15 -0700

    add m2rnn
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/math.py
+++ lm_engine/math.py
@@ -7,6 +7,22 @@ def ceil_divide(x: int, y: int) -> int:
     return (x + y - 1) // y
 
 
+def divide_if_divisible(dividend: int, divisor: int, msg: str = "") -> int:
+    """divide if divisible else raise an error
+
+    Args:
+        dividend (int): dividend
+        divisor (int): divisor
+        msg (str): error message
+
+    Returns:
+        int: result
+    """
+
+    assert dividend % divisor == 0, msg
+    return dividend // divisor
+
+
 def check_power_of_2(n: int) -> bool:
     return n & (n - 1) == 0 and n != 0
 
--- lm_engine/utils/__init__.py
+++ lm_engine/utils/__init__.py
@@ -5,11 +5,11 @@
 
 import torch
 
+from ..math import divide_if_divisible
 from .cpp_extension import compile_cpp_extension
 from .environment import environment, get_boolean_env_variable
 from .generation_cache import disable_generation_cache, is_generation_cache_enabled
 from .hf_hub import download_repo
-from .miscellaneous import divide_if_divisible
 from .mixed_precision import normalize_dtype_string, string_to_torch_dtype, torch_dtype_to_string
 from .packages import (
     is_aim_available,
