b4e1e0897c24d79d78ddb2d7666b16cdaa863e16
Mayank Mishra <mayank31398@gmail.com>
2026-09-07 15:19:59 -0700

    blue
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/kernels/triton_utils/__init__.py
+++ lm_engine/kernels/triton_utils/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2026, Mayank Mishra
 # **************************************************
 
-from ...math import compute_p_norm
 from .activations import (
     clamp,
     leaky_relu,
@@ -17,3 +16,4 @@ from .activations import (
 from .cu_seqlens import get_start_end
 from .elementwise import elementwise_2d_kernel, get_elementwise_2d_configs
 from .matmul import matmul
+from .norm import compute_p_norm
