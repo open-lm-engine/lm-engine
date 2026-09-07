4f11860f90948a29d012e78d36de4f4853da5544
Mayank Mishra <mayank31398@gmail.com>
2026-09-06 21:54:49 -0700

    blue
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/kernels/functional/swiglu/cuda_implementation/__init__.py
+++ lm_engine/kernels/functional/swiglu/cuda_implementation/__init__.py
@@ -15,9 +15,9 @@ class _SwigluCUDA(torch.autograd.Function):
     def forward(ctx, g: torch.Tensor, u: torch.Tensor) -> torch.Tensor:
         g = g.contiguous()
         u = u.contiguous()
-        ctx_save_for_backward(ctx, g, u)
-
         y = torch.empty_like(g, memory_format=torch.contiguous_format)
+
+        ctx_save_for_backward(ctx, g, u)
         _swiglu_forward_cuda(g=g, u=u, y=y)
 
         return y
@@ -25,10 +25,11 @@ class _SwigluCUDA(torch.autograd.Function):
     @staticmethod
     def backward(ctx, dy: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         g, u = ctx.saved_tensors
-        dy = dy.contiguous()
 
+        dy = dy.contiguous()
         dg = torch.empty_like(g, memory_format=torch.contiguous_format)
         du = torch.empty_like(u, memory_format=torch.contiguous_format)
+
         _swiglu_backward_cuda(g=g, u=u, dy=dy, dg=dg, du=du)
 
         return dg, du
@@ -37,9 +38,9 @@ class _SwigluCUDA(torch.autograd.Function):
 class _SwigluPackedCUDA(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x: torch.Tensor) -> torch.Tensor:
-        ctx_save_for_backward(ctx, x)
-
         y = torch.empty(*x.size()[:-1], divide_if_divisible(x.size(-1), 2), device=x.device, dtype=x.dtype)
+
+        ctx_save_for_backward(ctx, x)
         _swiglu_packed_forward_cuda(x=x, y=y)
 
         return y
@@ -47,8 +48,9 @@ class _SwigluPackedCUDA(torch.autograd.Function):
     @staticmethod
     def backward(ctx, dy: torch.Tensor) -> torch.Tensor:
         x = ctx.saved_tensors[0]
-
+        dy = dy.contiguous()
         dx = torch.empty_like(x, memory_format=torch.contiguous_format)
+
         _swiglu_packed_backward_cuda(x=x, dy=dy, dx=dx)
 
         return dx
