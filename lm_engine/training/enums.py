de118129a60855f1456bef57f5b30ea80e58d97d
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2026-09-10 14:39:00 -0700

    [AIM] drop aim (#508)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/training/enums.py
+++ lm_engine/training/enums.py
@@ -51,13 +51,6 @@ class KLDivergenceMethod(Enum):
     backward = "backward"
 
 
-class ExperimentsTrackerName(Enum):
-    """Experiment tracker to use"""
-
-    aim = "aim"
-    wandb = "wandb"
-
-
 class Kernel(Enum):
     # XMA (https://github.com/open-lm-engine/accelerated-model-architectures)
     causal_conv1d = "causal_conv1d"
