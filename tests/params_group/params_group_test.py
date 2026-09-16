afd59eb7422782837359adc1cce6379219682afa
Mayank Mishra <mayank31398@gmail.com>
2026-09-15 17:25:44 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/params_group/params_group_test.py
+++ tests/params_group/params_group_test.py
@@ -9,7 +9,7 @@ import pytest
 import torch
 from pydantic import ValidationError
 
-from lm_engine.training.arguments import ModuleParameterMatch, ParamsGroup
+from lm_engine.training.arguments import ModuleParameterMatch, OptimizerArgs, ParamsGroup
 from lm_engine.training.distributed import wrap_model_container_for_distributed_training
 from lm_engine.training.model_wrapper import get_model_container
 from lm_engine.training.optimization.params_group import get_param_groups_list
@@ -18,47 +18,12 @@ from lm_engine.training.parallel import ProcessGroupManager
 from ..utils import load_training_args_for_unit_tests
 
 
-_MUP_GROUP = ParamsGroup(
-    name="mup",
-    patterns=[
-        "*.mlp_block.c_fc.weight",
-        "*.mlp_block.c_proj.weight",
-        "*.mlp_block.c_fc_shared.weight",
-        "*.mlp_block.c_proj_shared.weight",
-        "*.mlp_block.gate.weight",
-        "*.mlp_block.up_fc.weight",
-        "*.mlp_block.gate_fc.weight",
-        "*.sequence_mixer.c_attn.weight",
-        "*.sequence_mixer.c_proj.weight",
-        "*.sequence_mixer.conv1d.weight",
-        "*.sequence_mixer.in_proj.weight",
-        "*.sequence_mixer.out_proj.weight",
-        "*.sequence_mixer.input_projection.weight",
-        "*.sequence_mixer.output_projection.weight",
-        "*.sequence_mixer.state_weight",
-        "*.sequence_mixer.D",
-        "*.sequence_mixer.decay_gate.A_log",
-    ],
-)
+_PARAM_GROUPS_CONFIG_DIR = os.path.join(os.path.dirname(__file__), "..", "..", "configs", "common", "param_groups")
 
-_NO_WEIGHT_DECAY_GROUP = ParamsGroup(
-    name="no_weight_decay",
-    module_matches=[
-        ModuleParameterMatch(class_name="RMSNorm", parameter_names=["weight"]),
-        ModuleParameterMatch(class_name="LayerNorm", parameter_names=["weight"]),
-        ModuleParameterMatch(class_name="PNorm", parameter_names=["weight"]),
-        ModuleParameterMatch(class_name="RNN", parameter_names=["state_weight"]),
-        ModuleParameterMatch(class_name="GRU", parameter_names=["state_weight"]),
-        ModuleParameterMatch(class_name="M2RNN", parameter_names=["state_weight"]),
-        ModuleParameterMatch(class_name="Mamba2", parameter_names=["D"]),
-        ModuleParameterMatch(class_name="SoftplusDecayGate", parameter_names=["A_log", "dt_bias"]),
-    ],
-    patterns=["*.bias"],
-    params_group_kwargs={"weight_decay": 0},
-)
 
-# catch-all: get_param_groups_with_names raises if any parameter matches no group's patterns
-_NORMAL_GROUP = ParamsGroup(name="normal", patterns=["*"])
+def _load_param_groups(filename: str) -> list[ParamsGroup]:
+    # exercise the actual shipped configs, so a regression there is also caught here
+    return OptimizerArgs(param_groups=os.path.join(_PARAM_GROUPS_CONFIG_DIR, filename)).param_groups
 
 
 @pytest.mark.parametrize("use_fsdp", [False, True])
@@ -67,8 +32,8 @@ _NORMAL_GROUP = ParamsGroup(name="normal", patterns=["*"])
 @pytest.mark.parametrize(
     "filename_param_groups",
     [
-        ("mup.json", [_MUP_GROUP, _NO_WEIGHT_DECAY_GROUP, _NORMAL_GROUP]),
-        ("normal.json", [_NO_WEIGHT_DECAY_GROUP, _NORMAL_GROUP]),
+        ("mup.json", _load_param_groups("mup.yml")),
+        ("normal.json", _load_param_groups("normal.yml")),
     ],
 )
 def test_params_group(
